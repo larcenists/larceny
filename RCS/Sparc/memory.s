a88d7609c27fe8844692e9316e859395ed4d5a83
lth <empty>
1991-06-24 01:55:40 +0000

    Calling conventions changed; internal 'gcstart' factored out.

--- RCS/Sparc/memory.s
+++ RCS/Sparc/memory.s
@@ -1,48 +1,48 @@
-! Assembly-language routines for allocation and mutation.
+! Assembly-language millicode routines for allocation and mutation.
 ! Sparc version.
-! All code goes into this file; definitions are in "memory.s.h"
 !
-! $Id: memory.s,v 1.2 91/06/17 18:52:17 lth Exp Locker: lth $
+! $Id: memory.s,v 1.3 91/06/21 15:16:39 lth Exp Locker: lth $
 !
 ! This file defines the following builtins:
 !
-!   alloc( n )		  allocate n uninitialized words
-!   alloci( n, v )	  allocate n words initialized to v
-!   setcar( p, v )	  set to v the car field of the pair pointed to by p
-!   setcdr( p, v )	  set to v the cdr field of the pair pointed to by p
-!   vectorset( p, i, v )  set to v the ith slot of the vector pointed to by p
-!   gcstart( n )	  Saves the virtual machine state in a predictable
+!   _alloc( n )		  allocate n uninitialized words
+!   _alloci( n, v )	  allocate n words initialized to v
+!   _setcar( p, v )	  set to v the car field of the pair pointed to by p
+!   _setcdr( p, v )	  set to v the cdr field of the pair pointed to by p
+!   _vectorset( p, i, v ) set to v the ith slot of the vector pointed to by p
+!   _gcstart( n )	  Saves the virtual machine state in a predictable
 !			  place, flushes stack, calls collector, syncs the
 !			  cache, and allocates the requested number of words,
 !			  returning a pointer to those words. If n is the
 !			  fixnum -1, then no memory is allocated; rather,
 !			  a tenuring collection is invoked. (The default is
 !			  to do an ephemeral collection.)
-!   stkuflow()		  Procedure to be called on a stack cache underflow.
+!   _stkuflow()		  Procedure to be called on a stack cache underflow.
 !			  It will restore a single continuation frame from
 !			  the heap-based continuations, if there are any.
 !			  After restoring the frame, it will branch to the
 !			  return address in the frame.
-!   stkoflow()		  Procedure to be called on a stack cache overflow.
+!   _stkoflow()		  Procedure to be called on a stack cache overflow.
 !			  It will flush the stack cache to memory and setup
 !			  the continuation pointer, and return to its caller.
 !
-! 'gcstart' is made public for use by open-coded 'cons' calls and can also
+! '_gcstart' is made public for use by open-coded 'cons' calls and can also
 ! be used to implement a user-level procedure which invokes the collector;
 ! to do an ephemeral collection, simply request to allocate 0 bytes.
 !
 ! Arguments are always passed in registers RESULT, ARGREG2, and ARGREG3, all
 ! of which are rootable. The result, if any, is returned in register RESULT.
-! If no result is required, RESULT remains untouched. ARGREG2 and ARGREG2 are
+! If no result is required, RESULT is set to 0. ARGREG2 and ARGREG2 are
 ! never destroyed by the call.
+! On entry, %o7 must contain the return address, and %R0 must contain the
+! pointer to the calling procedure.
 !
-! THIS CODE USES AND CHANGES %TMP0 AND %TMP1, BOTH OF WHICH MUST BE 
-! NONROOTABLE.
+! Internal calling conventions are somewhat odd; before a call to 'addtrans'
+! or 'gcstart', the "external" return address must be saved in %TMP0, where
+! the internal procedures will expect to find it, in case it must be adjusted
+! due to a collection.
 !
-! %GLOBALS should never be allocated to an %o register! If it is, some of 
-! these procedures will not work right.
-!
-! Assemble with 'as -P' to run the preprocessor before the assembler.
+! Assemble with 'as -P'.
 
 #include "memory.s.h"
 
@@ -60,13 +60,13 @@
 #define TAG_PAIR	0x01
 #define TAG_VEC		0x03
 
-	.globl alloc, alloci, setcar, setcdr, vectorset, gcstart
-	.globl stkoflow, stkuflow
+	.global _alloc, _alloci, _setcar, _setcdr, _vectorset, _gcstart
+	.global _stkoflow, _stkuflow
 
-	.text
+	.seg "text"
 
 !-----------------------------------------------------------------------------
-! 'alloc' takes one parameter, a fixnum which is the number of words to
+! '_alloc' takes one parameter, a fixnum which is the number of words to
 ! allocate. It returns an untagged pointer to this many words.
 !
 ! Note that the delayed roundup (i.e. done after test for overflow) makes
@@ -81,23 +81,28 @@
 !   return p;
 ! }
 
-alloc:
+_alloc:
 	add	%E_TOP, %RESULT, %E_TOP		! allocate optimistically
 	and	%RESULT, 0x04, %TMP0		! get 'odd' bit
 	cmp	%E_TOP, %E_LIMIT		! check for overflow
 	blt,a	Lalloc1				! skip of no overflow
 	sub	%E_TOP, %RESULT, %RESULT	! setup result
+
+	! Overflow; need to collect.
+
+	mov	%o7, %TMP0			! Save old return address
 	call	gcstart				! deal with overflow
 	nop
-	retl
+	jmp	%TMP0+8				! return to Scheme code
 	nop
+
 Lalloc1:
 	retl
 	add	%E_TOP, %TMP0, %E_TOP		! round up
 
 
 !-----------------------------------------------------------------------------
-! 'alloci' takes two parameters, a fixnum which is the number of words to
+! '_alloci' takes two parameters, a fixnum which is the number of words to
 ! allocate, and the value (a word) with which to initialize the memory.
 ! It returns an untagged pointer to the requested number of words.
 !
@@ -116,7 +121,7 @@ Lalloc1:
 !   return p;
 ! }
 
-alloci:
+_alloci:
 	mov	%RESULT, %TMP0			! count into TMP0
 	mov	%E_TOP, %RESULT			! resulting pointer
 	add	%E_TOP, %TMP0, %E_TOP		! allocate optimistically
@@ -124,8 +129,14 @@ alloci:
 	cmp	%E_TOP, %E_LIMIT		! check for overflow
 	ble,a	Lalloci1			! no overflow, start init
 	add	%E_TOP, %TMP1, %E_TOP		! adjust if necessary
-	call	gcstart				! deal with overflow
+
+	! Overflow; need to collect
+
 	mov	%TMP0, %RESULT			! restore count for argument
+	mov	%o7, %TMP0
+	call	gcstart				! deal with overflow
+	nop
+	mov	%TMP0, %o7			! restore return address
 
 	! restore byte/word count for use in initialization
 
@@ -149,7 +160,7 @@ Lalloci2:
 
 
 !-----------------------------------------------------------------------------
-! 'setcar' takes two parameters: a tagged pointer, which is assumed to point
+! '_setcar' takes two parameters: a tagged pointer, which is assumed to point
 ! to a pair, and a value, and sets the 'car' cell of the pair to the value.
 !
 ! If the pair is in the tenured space, then a transaction must be added
@@ -161,21 +172,25 @@ Lalloci2:
 !   *ptrof( p ) = v;
 ! }
 
-setcar:
+_setcar:
 	ld	[ %GLOBALS+T_BASE_OFFSET ], %TMP0	! fetch tenured base
 	xor	%RESULT, TAG_PAIR, %TMP1	! strip tag
 	cmp	%TMP1, %TMP0
 	blt	Lsetcar1
-	nop
+	mov	%o7, %TMP0
+
+	! Must add transaction to list
+
 	call	addtrans			! add transaction to list
 	nop
+
 Lsetcar1:
-	retl
-	st	%ARGREG2, [%ARGREG1+CAR_OFFSET]	! CAR_OFFSET compensates right
+	jmp	%TMP0+8
+	st	%ARGREG2, [%RESULT+CAR_OFFSET]	! CAR_OFFSET compensates right
 
 
 !-----------------------------------------------------------------------------
-! 'setcdr' takes two parameters: a tagged pointer, which is assumed to point
+! '_setcdr' takes two parameters: a tagged pointer, which is assumed to point
 ! to a pair, and a value, and sets the 'cdr' cell of the pair to the value.
 !
 ! If the pair is in the tenured space, then a transaction must be added
@@ -187,21 +202,23 @@ Lsetcar1:
 !   *(ptrof( p )+4) = v;
 ! }
 
-setcdr:
+_setcdr:
 	ld	[%GLOBALS+T_BASE_OFFSET], %TMP0 ! fetch tenured base
 	xor	%RESULT, TAG_PAIR, %TMP1	! strip tag
 	cmp	%TMP1, %TMP0
 	blt	Lsetcdr1
-	nop
+	mov	%o7, %TMP0
+
 	call	addtrans			! add transaction to list
 	nop
+
 Lsetcdr1:
-	retl
-	st	%ARGREG2, [%ARGREG1+CDR_OFFSET]	! CDR_OFFSET compensates right
+	jmp	%TMP0+8
+	st	%ARGREG2, [%RESULT+CDR_OFFSET]	! CDR_OFFSET compensates right
 
 
 !-----------------------------------------------------------------------------
-! 'vectorset' takes three parameters: a tagged pointer, which is assumed to
+! '_vectorset' takes three parameters: a tagged pointer, which is assumed to
 ! point to a vector, a fixnum index, which is assumed to be valid for the
 ! given vector, and a value, and sets the specified slot of the vector to
 ! the value.
@@ -215,23 +232,92 @@ Lsetcdr1:
 !   *(ptrof( p )+i+4) = v;			; compensate for header
 ! }
 
-vectorset:
+_vectorset:
 	ld	[%GLOBALS+T_BASE_OFFSET], %TMP0 ! fetch tenured base
-	xor	%RESULT, TAG_VEC, %TMP1	! strip tag
+	xor	%RESULT, TAG_VEC, %TMP1		! strip tag
 	cmp	%TMP1, %TMP0
 	blt	Lvectorset1			! not in tenured space
-	nop
+	mov	%o7, %TMP0
+
 	call	addtrans
 	nop
+
 Lvectorset1:
-	add	%RESULT, %ARGREG2, %TMP0	! pointer which does not
+	add	%RESULT, %ARGREG2, %TMP1	! pointer which does not
 						! compensate for header
 						! or tag
-	retl
-	st	%ARGREG3, [%TMP0+VEC_OFFSET]	! VEC_OFFSET compensates
+	jmp	%TMP0+8
+	st	%ARGREG3, [%TMP1+VEC_OFFSET]	! VEC_OFFSET compensates
 
 
 !-----------------------------------------------------------------------------
+! '_gcstart' merely calls 'gcstart', with a bit of protocol.
+
+_gcstart:
+	mov	%o7, %TMP0
+	call	gcstart
+	nop
+	jmp	%TMP0+8
+	nop
+
+
+!-----------------------------------------------------------------------------
+! '_stkuflow' is designed to be returned through on a stack cache underflow.
+! The address of '_stkuflow' should be in a dummy continuation at the bottom
+! of the stack (top of the stack cache). On a return which underflows the
+! stack cache, '_stkuflow' is entered. It restores a single continuation frame
+! and jumps to the return address in the newly restored frame.
+
+_stkuflow:
+	st	%STKP, [ %GLOBALS+SP_OFFSET ]
+
+	save	%sp, -96, %sp
+	call	_restore_frame
+	nop
+	restore
+
+	ld	[ %GLOBALS+SP_OFFSET ], %STKP
+	ld	[ %STKP ], %TMP0
+	jmp	%TMP0+8
+	nop
+
+
+!-----------------------------------------------------------------------------
+! '_stkoflow' handles stack overflow. When the mutator detects stack overflow,
+! then '_stkoflow' should be called. It flushes the stack cache and invokes
+! the garbage collector if necessary, and then returns to its caller.
+
+_stkoflow:
+	st	%E_TOP, [ %GLOBALS+E_TOP_OFFSET ]
+	st	%STKP, [ %GLOBALS+SP_OFFSET ]
+
+	save	%sp, -96, %sp
+	call	_flush_stack_cache
+	nop
+	restore
+
+	ld	[ %GLOBALS+SP_OFFSET ], %STKP
+	ld	[ %GLOBALS+E_TOP_OFFSET ], %E_TOP
+	cmp	%E_TOP, %E_LIMIT
+	blt	Lstkoflow1
+	mov	%o7, %TMP0
+
+	! Overflow; must collect
+
+	call	gcstart
+	nop
+
+Lstkoflow1:
+	jmp	%TMP0+8
+	nop
+
+
+!-----------------------------------------------------------------------------
+! 'gcstart'
+!
+! On entry, %TMP0 has return address pointing to Scheme code, and %REG0 is
+! assumed to have a valid procedure pointer.
+!
 ! 'gcstart' saves the state and invokes the collector. It also takes an
 ! argument, a fixnum indicating the number of words that was attempted 
 ! allocated when the heap overflow occured. If the overflow was due to
@@ -247,6 +333,15 @@ Lvectorset1:
 ! (if not 0xFFFFFFFC) can safely be allocated.
 
 gcstart:
+	! Setup a continuation
+
+	sub	%STKP, 16, %STKP
+	st	%TMP0, [ %STKP+0 ]	! return address
+	mov	16, %TMP0
+	st	%TMP0, [ STKP+4 ]	! size
+	st	%R0, [ STKP+8 ]		! procedure
+	st	%g0, [ STKP+12 ]	! dummy
+	
 	! Save context
 
 	st	%REG0, [ %GLOBALS+REG0_OFFSET ]
@@ -257,7 +352,7 @@ gcstart:
 	st	%REG5, [ %GLOBALS+REG5_OFFSET ]
 	st	%REG6, [ %GLOBALS+REG6_OFFSET ]
 	st	%REG7, [ %GLOBALS+REG7_OFFSET ]
-	! temps ?
+
 	st	%ARGREG2, [ %GLOBALS+ARGREG2_OFFSET ]
 	st	%ARGREG3, [ %GLOBALS+ARGREG3_OFFSET ]
 	st	%RESULT, [ %GLOBALS+RESULT_OFFSET ]
@@ -265,13 +360,15 @@ gcstart:
 
 	! C-language call
 
-	mov	%RESULT, %o0
+	save	%sp, -96, %sp
 	call	_gcstart2
-	nop
+	mov	%RESULT, %o0
+	restore
 
 	! Restore context
 
 	set	_globals, %GLOBALS			! this is fundamental
+	set	_millicode, %MILLICODE
 	
 	ld	[ %GLOBALS+REG0_OFFSET ], %REG0
 	ld	[ %GLOBALS+REG1_OFFSET ], %REG1
@@ -281,7 +378,7 @@ gcstart:
 	ld	[ %GLOBALS+REG5_OFFSET ], %REG5
 	ld	[ %GLOBALS+REG6_OFFSET ], %REG6
 	ld	[ %GLOBALS+REG7_OFFSET ], %REG7
-	! temps?
+
 	ld	[ %GLOBALS+ARGREG2_OFFSET ], %ARGREG2
 	ld	[ %GLOBALS+ARGREG3_OFFSET ], %ARGREG3
 	ld	[ %GLOBALS+RESULT_OFFSET ], %RESULT
@@ -302,124 +399,43 @@ gcstart:
 	add	%E_TOP, %TMP0, %E_TOP
 	
 Lgcstart1:
-	retl
-	nop
 
+	! Destroy continuation and return
 
-!-----------------------------------------------------------------------------
-! 'stkuflow' is designed to be returned through on a stack cache underflow.
-! The address of 'stkuflow' should be in a dummy continuation at the bottom
-! of the stack (top of the stack cache). On a return which underflows the
-! stack cache, 'stkuflow' is entered. It restores a single continuation frame
-! and jumps to the return address in the newly restored frame.
+	ld	[ %STKP+0 ], %TMP0		! restore return address
+	retl					! return to millicode
+	add	%STKP, 16, %STKP		! deallocate frame
 
-stkuflow:
-	std	%o0, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET ]
-	std	%o2, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+8 ]
-	std	%o4, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+16 ]
-	std	%o6, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+24 ]
-
-	call	_restore_frame
-	st	%SP, [ %GLOBALS+SP_OFFSET ]
-
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET ], %o0
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+8 ], %o2
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+16 ], %o4
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+24 ], %o6
-
-	ld	[ %GLOBALS+SP_OFFSET ], %SP
-	ld	[ %SP ], %TMP0
-	jump	%TMP0+8
-	nop
-
-
-!-----------------------------------------------------------------------------
-! 'stkoflow' handles stack overflow. When the mutator detects stack overflow,
-! then 'stkoflow' should be called. It flushes the stack cache and invokes
-! the garbage collector if necessary, and then returns to its caller.
-
-stkoflow:
-	std	%o0, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET ]
-	std	%o2, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+8 ]
-	std	%o4, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+16 ]
-	std	%o6, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+24 ]
-
-	call	_flush_stack_cache
-	st	%SP, [ %GLOBALS+SP_OFFSET ]
-
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET ], %o0
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+8 ], %o2
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+16 ], %o4
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+24 ], %o6
-
-	ld	[ %GLOBALS+SP_OFFSET ], %sp
-	cmp	%E_TOP, %E_LIMIT
-	blt	Lstkoflow1
-	nop
-	call	gcstart
-	nop
-Lstkoflow1:
-	retl
-	nop
-
-
+	
 !-----------------------------------------------------------------------------
 ! 'addtrans' takes one parameter, which must be a tagged pointer, and adds it
 ! to the transaction list. If the transaction list is full (i.e. there is
-! an overflow of tenured space (shudder)) then 'addtrans' will call the C
-! procedure '_compact_trans', which compacts the transaction list. If
-! '_compact_trans' determines that the compaction did not help sufficiently,
-! it returns a 0 value, and 'addtrans' must trigger a tenuring collection.
-! After the collection there is no sense in adding the transaction to the
-! list.
+! an overflow of tenured space (shudder)) then 'addtrans' will simply perform
+! a tenuring collection.
+! 
+! A more sophisticated approach would be to attempt to compact the transaction
+! list before resorting to collection; this should probably be investigated.
 !
-! Otherwise, '_compact_trans' returns a nonzero value, and 'addtrans' can
-! go ahead and add the transaction.
-!
-! Big Q: Do we need to save the context before we call _compact_trans?
-!        It probebly depends on what is in the %o registers; presumably
-!        we can get away with just saving those.
+! On entry, the "external" return address is in %TMP0.
 
 addtrans:
 
 	! Get tenured-space limits and check for overflow
 
-	ld	[ %GLOBALS+T_ENTRIES_OFFSET ], %TMP0
-	ld	[ %GLOBALS+T_TOP_OFFSET ], %TMP1
-	cmp	%TMP0, %TMP1
+	ld	[ %GLOBALS+T_ENTRIES_OFFSET ], %TMP1
+	ld	[ %GLOBALS+T_TOP_OFFSET ], %TMP2
+	cmp	%TMP1, %TMP2
 	bgt	Laddtrans1
-	sub	%TMP0, 4, %TMP1
-
-	! %GLOBALS had better not be a %o register!
-
-	std	%o0, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET ]
-	std	%o2, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+8 ]
-	std	%o4, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+16 ]
-	std	%o6, [ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+24 ]
-
-	call	_compact_trans
-	nop
-
-	tst	%C_RESULT
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET ], %o0
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+8 ], %o2
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+16 ], %o4
-	ldd	[ %GLOBALS+SAVED_OUTPUT_REGS_OFFSET+24 ], %o6
-
-	bne	Laddtrans1
-	sub	%TMP0, 4, %TMP1
+	sub	%TMP1, 4, %TMP2
 
 	! We've lost. Go ahead and collect.
 
+	b	gcstart
 	set	0xFFFFFFFC, %RESULT
-	call	gcstart
-	nop
-	retl
-	nop
 
 Laddtrans1:
-	st	%RESULT, [ %TMP0+0 ]
+	st	%RESULT, [ %TMP1 ]
 	retl
-	st	%TMP1, [ %GLOBALS+T_ENTRIES_OFFSET ]
+	st	%TMP2, [ %GLOBALS+T_ENTRIES_OFFSET ]
 
 	! end of file
