0cacaf23f9d351b0a5237f15d33de74563b1c3f4
lth <empty>
1991-06-26 12:46:11 +0000

    generics, new exceptions.

--- RCS/Bench/bench2.s
+++ RCS/Bench/bench2.s
@@ -1,5 +1,5 @@
 ! -*- Fundamental -*-
-! $Id: bench2.s,v 1.2 91/06/24 11:21:27 lth Exp Locker: lth $
+! $Id: bench2.s,v 1.3 91/06/24 13:04:52 lth Exp Locker: lth $
 !
 ! Hand-compiled code for the following program:
 !
@@ -37,16 +37,19 @@ L1:
 	mov	16, %g1					! frame size
 	beq	L2
 	std	%g0, [ %STKP+0 ]			! save retn & fsize
-	ld	[ %MILLICODE+M_WRONGARGS ], %g1
-	jmpl	%g1, %o7
-	nop
+	call	Lexception
+	add	%o7, (L1-(.-4))-8, %o7
 
 	! Real procedure starts here
 L2:
 	tsubcc	%REG1, %g0, %g0				! (= n 0) ?
 	bvc	L3
 	nop						! Can't fill this
-	! ...						! Generic case
+	! Generic case
+	ld	[ %MILLICODE + M_ZEROP ], %TMP0
+	jmpl	%TMP0, %o7
+	mov	%REG1, %RESULT
+	cmp	%RESULT, TRUE_CONST			! Set condition code
 L3:
 	bne,a	L4
 	tsubcc	%REG1, 4, %TMP0				! n - 1
@@ -60,30 +63,34 @@ L5:
 L4:	! 'False' case
 	bvc,a	L6
 	mov	%TMP0, %REG1				! setup argument
-	! ...						! Generic case
-
+	! Generic case
+	ld	[ %MILLICODE + M_SUB ], %TMP0
+	mov	%REG1, %RESULT
+	jmpl	%TMP0, %o7
+	mov	4, %ARGREG2
+	mov	%RESULT, %REG1
 
 L6:	! Now do a tail call to 'loop3'
-	! ...						! %REG0 <-- 'loop3'
+	! ...						! %REG0 <-- loop3
 	and	%REG0, TAGMASK, %TMP1
 	cmp	%TMP1, PROCTAG				! procedure?
 	beq,a	L7
 	deccc	%TIMER					! Count down timer
-	call	L8
+	call	Lexception
 	add	%o7, (L6-(.-4))-8, %o7			! Return to L6
 L7:
 	bne	L9					! Timer zero?
 	mov	4, %RESULT				! arg count
-	call	L8
+	call	Lexception
 	add	%o7, (L9-(.-4))-8, %o7			! Return to L9
 L9:
-	ld	[ %TMP0+... ], %TMP1			! get code vector
-	jmp	%TMP1+...				! invoke procedure
+	ld	[ %TMP0+CODEVECOR ], %TMP1		! get code vector
+	jmp	%TMP1+CODEOFFSET			! invoke procedure
 	add	%STKP, 16, %STKP			! deallocate frame
 
 ! Exception handler for timer/non-procedure
 
-L8:
+Lexception:
 	ld	[ %MILLICODE+M_EXCEPTION ], %TMP0
 	jmp	%TMP0
 	nop
