57ab3384ff05b9e3c601bd73ac0807c616a6c80e
lth <empty>
1991-06-26 16:44:02 +0000

    Initial revision

---
+++ RCS/Sys/Old/machine.h
@@ -0,0 +1,14 @@
+/* -*- Fundamental -*-
+ *
+ * Scheme Run-time System
+ *
+ * Machine selection header file.
+ *
+ * $Id$
+ */
+
+/* Let's hope that Sparc1 == Sparc2, always... */
+
+#define SPARC1		1		/* Sparc 1, 1+, SLC, IPC */
+#define SPARC2		1		/* Sparc 2 */
+#define SYMMETRY	0		/* Sequent Symmetry, 80386 */
---
+++ RCS/Sys/main.h
@@ -0,0 +1,11 @@
+/* -*- Fundamental -*-
+ *
+ * Scheme Run-time System
+ * Procedures and variables defined globally in "main.c".
+ *
+ * $Id$
+ */
+ 
+extern word globals[];
+extern void (*millicode[])();
+extern int panic();
