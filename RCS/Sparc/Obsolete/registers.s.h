346ff7f03d4fe44e0ffeefa7abd9818f6db40862
lth <empty>
1991-06-27 16:34:09 +0000

    Had to change to accomodate previous-frame's stack pointer.

--- RCS/Sparc/Obsolete/registers.s.h
+++ RCS/Sparc/Obsolete/registers.s.h
@@ -1,7 +1,7 @@
 ! Symbolic register definitions.
 ! Sparc version.
 !
-! $Id: registers.s.h,v 1.1 91/06/20 15:49:02 lth Exp Locker: lth $
+! $Id: registers.s.h,v 1.2 91/06/26 16:46:26 lth Exp Locker: lth $
 
 #define E_TOP		o0
 #define E_LIMIT		o1
@@ -13,26 +13,26 @@
 ! g1 *must* be a temp register
 #define TMP0		g1
 
-#define TIMER		l0
-#define TMP1		l1
-#define TMP2		l2
-#define TMP3		l3
-#define TMP4		l4
-#define TMP5		l5
-#define MILLICODE	l6
-#define GLOBALS		l7
+#define TIMER		i0
+#define TMP1		i1
+#define TMP2		i2
+#define TMP3		i3
+#define TMP4		i4
+#define MILLICODE	i5
+#define GLOBALS		i7
 
-#define REG0		i0
-#define REG1		i1
-#define REG2		i2
-#define REG3		i3
-#define REG4		i4
-#define REG5		i5
-#define REG6		i6
-#define REG7		i7
+#define REG0		l0
+#define REG1		l1
+#define REG2		l2
+#define REG3		l3
+#define REG4		l4
+#define REG5		l5
+#define REG6		l6
+#define REG7		l7
 
 ! C language registers; use with care.
 
+#define C_PREV_SP       i6
 #define C_RESULT        o0
 #define C_SP		o6
 #define C_RETADDR	o7
