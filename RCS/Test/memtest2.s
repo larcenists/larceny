0fafd1470d566343a27bd8c49ee2163be1191dc7
lth <empty>
1991-07-10 18:24:37 +0000

    much simplified -- now only has millicall().

--- RCS/Test/memtest2.s
+++ RCS/Test/memtest2.s
@@ -1,76 +1,46 @@
 ! Memory management test program
 ! Sparc assembly language procedures
 !
-! $Id$
+! $Id: memtest2.s,v 1.1 91/06/27 16:35:05 lth Exp Locker: lth $
 !
 ! All public procedures here are called from C.
 	
-	.global	_test_alloc_2, _test_alloci_2, _test_gcstart_2
-	.global _test_stkoflow_2
+#define ASSEMBLY
+#include "registers.s.h"
+#include "millicode.h"
+
+	.global	_millicall
 
 	.seg "text"
 
-! Test the alloc() millicode procedure.
-! Takes an argument which is the number of words to allocate.
-! Returns untagged pointer to allocated space.
+! Generalized interface to calling millicode from C
 !
-! We load %REG0 with a dummy '0' procedure (see doc'n; this is a special
-! case procedure that is not clobbered by collection), then allocate,
-! and then return the allocated pointer.
-!
-! This procedure is possibly reentrant, but I have to convince myself...
-! Anyway it's a good example of C calling Scheme.
+! word millicall( word (*proc)(), word result, word arg2, word arg3 )
 
-_test_alloc_2:
-
-	!---------
-	! C-to-Scheme Prologue
-	save	%sp, -72, %sp
-	st	%i0, [ %fp+8 ]			! save argument
-	st	%i7, [ %sp+4 ]			! save return address
-
-	! Initialize virtual machine from "globals" array
-
+_millicall:
+	save	%sp, -96, %sp
+	st	%i0, [ %fp+44 ]
+	st	%i1, [ %fp+48 ]
+	st	%i2, [ %fp+52 ]
+	st	%i3, [ %fp+56 ]
+	st	%i7, [ %fp+60 ]
 	call	_restore_scheme_context
 	nop
-	! End prologue
-	!---------
 
-	mov	0, %REG0			! fake procedure
-	ld	[ %MILLICODE + M_ALLOC ], %TMP0
+	mov	0, %REG0
+	ld	[ %fp+44 ], %TMP0
+	ld	[ %fp+48 ], %RESULT
+	ld	[ %fp+52 ], %ARGREG2
+	ld	[ %fp+56 ], %ARGREG3
 	jmpl	%TMP0, %o7
-	ld	[ %fp+8 ], %RESULT		! argument
-
-	!---------
-	! C-to-Scheme Epilogue
-	! Save virtual machine state in "globals" array
+	nop
 
 	call	_save_scheme_context
 	nop
 
-	! Get ready to return to C
-
-	mov	%RESULT, %i0			! return value
-	ld	[ %sp+4 ], %i7			! restore return address
+	mov	%RESULT, %i0
+	ld	[ %fp+60 ], %i7
 	ret
 	restore
-	! End epilogue
-	!---------
 
-
-_test_alloci_2:
-	retl
-	nop
-
-
-_test_gcstart_2:
-	retl
-	nop
-
-
-_test_stkoflow_2:
-	retl
-	nop
-
-
-	
+	! EOF
