3082a9a3f72a3be49ae4a8f66d5f09940741d1b9
lth <empty>
1991-07-12 03:12:36 +0000

    corrected erroneous definitions.

--- RCS/Sys/main.h
+++ RCS/Sys/main.h
@@ -3,9 +3,9 @@
  * Scheme Run-time System
  * Procedures and variables defined globally in "main.c".
  *
- * $Id$
+ * $Id: main.h,v 1.1 91/06/26 16:45:02 lth Exp Locker: lth $
  */
  
 extern word globals[];
-extern void (*millicode[])();
-extern int panic();
+extern word (*millicode[])();
+extern void panic();
