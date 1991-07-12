bacab0a5b2ae94b98c05f3f36b5591d41d245d7b
lth <empty>
1991-07-12 03:14:26 +0000

    Considerably cleaned up. This may well be the final version (modulo what S_ENTRY ends up being called.)

--- RCS/Sparc/Obsolete/schemestart.s
+++ RCS/Sparc/Obsolete/schemestart.s
@@ -2,68 +2,41 @@
 ! Sparc version.
 ! Scheme initialization file.
 !
-! $Id: schemestart.s,v 1.3 91/06/21 15:12:55 lth Exp Locker: lth $
+! $Id: schemestart.s,v 1.4 91/06/24 01:56:29 lth Exp Locker: lth $
 !
 ! The procedure _schemestart is called from the C-language initialization
-! code. _schemestart sets up the virtual machine by initializing %GLOBALS
-! and then fetching values from the globals table into the VM registers.
-! Finally, it calls the application specific startup procedure 'S_ENTRY'.
-! If 'S_ENTRY' returns, then _schemestart returns to its caller.
+! code. _schemestart sets up the virtual machine and then calls the
+! application specific startup procedure 'S_ENTRY'. If 'S_ENTRY' returns,
+! then _schemestart returns to its caller.
 ! 
+! If the C startup wishes to pass arguments to S_ENTRY, it should intialize
+! the appropriate register save areas in globals[].
+!
 ! Assemble with '-P' flag
 
-#include "memory.s.h"
+#include "registers.s.h"
 
-	.globl _schemestart
-	.text
+	.global	_schemestart
+
+	.seg	"text"
 
 _schemestart:
 	save	%sp, -96, %sp			! Standard stack frame
+	st	%i7, [ %fp + 0x44 ]
 
-	set	_globals, %GLOBALS
-
-	ld	[ %GLOBALS+E_TOP_OFFSET ], %E_TOP
-	ld	[ %GLOBALS+E_LIMIT_OFFSET ], %E_LIMIT
-
-	ld	[ %GLOBALS+STK_MAX_OFFSET ], %STKP
-	ld	[ %GLOBALS+INITIAL_TIMER_OFFSET ], %TIMER
-
-	! All rootable registers must be initialized
-
-	mov	%g0, %RESULT
-	mov	%g0, %ARGREG2
-	mov	%g0, %ARGREG3
-
-	ld	[ %GLOBALS+R0_OFFSET ], %R0
-	ld	[ %GLOBALS+R1_OFFSET ], %R1
-	ld	[ %GLOBALS+R2_OFFSET ], %R2
-	ld	[ %GLOBALS+R3_OFFSET ], %R3
-	ld	[ %GLOBALS+R4_OFFSET ], %R4
-	ld	[ %GLOBALS+R5_OFFSET ], %R5
-	ld	[ %GLOBALS+R6_OFFSET ], %R6
-	ld	[ %GLOBALS+R7_OFFSET ], %R7
-
-	! Initialize temporaries just because.
-
-	mov	%g0, %TMP0
-	mov	%g0, %TMP1
-	mov	%g0, %TMP2
-	mov	%g0, %TMP3
-	mov	%g0, %TMP4
-	mov	%g0, %TMP5
-	mov	%g0, %TMP6
+	call	_restore_scheme_context
+	nop
 
 ! Call application code: setup a minimal continuation and jump. The
 ! continuation contains a dummy procedure field; the stack flusher
 ! had better deal with this.
 
-	set	L1, %TMP0
-	st	%TMP0, [ %STKP-12 ]	! return address
-	mov	16, %TMP1
-	st	%TMP1, [ %STKP-8 ]	! size
-	st	%g0, [ %STKP-4 ]	! procedure (dummy!)
-	st	%g0, [ %STKP ]		! padding
+	set	L1-8, %TMP0		! this is ok since this code won't move
 	sub	%STKP, 16, %STKP	! allocate frame
+	st	%TMP0, [ %STKP ]	! return address
+	mov	12, %TMP1
+	st	%TMP1, [ %STKP+4 ]	! size
+	st	%g0, [ %STKP+8 ]	! procedure (dummy!)
 
 ! Do the call. We simply jump to an application-specific entry procedure
 ! S_ENTRY which must be present in one of the files of the Scheme application.
@@ -77,6 +50,10 @@ _schemestart:
 
 	! Return to C code
 L1:
+	call	_save_scheme_context
+	nop
+
+	ld	[ %fp + 0x44 ], %i7
 	ret
 	restore
 
