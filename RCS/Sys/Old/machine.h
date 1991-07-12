f75a0327095c6473a4cae011ec9d702917cb3a2f
lth <empty>
1991-07-12 03:13:41 +0000

    added generic SPARC definition.

--- RCS/Sys/Old/machine.h
+++ RCS/Sys/Old/machine.h
@@ -4,11 +4,13 @@
  *
  * Machine selection header file.
  *
- * $Id$
+ * $Id: machine.h,v 1.1 91/06/26 16:44:02 lth Exp Locker: lth $
  */
 
 /* Let's hope that Sparc1 == Sparc2, always... */
 
 #define SPARC1		1		/* Sparc 1, 1+, SLC, IPC */
-#define SPARC2		1		/* Sparc 2 */
+#define SPARC2		0		/* Sparc 2 */
 #define SYMMETRY	0		/* Sequent Symmetry, 80386 */
+
+#define SPARC	(SPARC1 || SPARC2)
