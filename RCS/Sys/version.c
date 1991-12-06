2fb44b4221aada3f3aff097f309924eb690d7a14
lth <empty>
1991-12-06 15:10:08 +0000

    How much can you update the version file!?

--- RCS/Sys/version.c
+++ RCS/Sys/version.c
@@ -1,9 +1,2 @@
-#ifdef __STDC__
-  #ifndef USER
-    #define USER "The Wizard of Oz"
-  #endif
-  char *version = "Larceny version 0.01, built by " USER " on " __DATE__;
-#else
-  char *version = "Larceny version 0.01";
-#endif
-
+/* $Id$ */
+char *version = "Larceny version 0.01";
