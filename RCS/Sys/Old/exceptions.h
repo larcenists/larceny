34c68900df4ea696c5060cb8f2dc6bd5f655c8f4
lth <empty>
1991-12-06 15:00:36 +0000

    Initial revision

---
+++ RCS/Sys/Old/exceptions.h
@@ -0,0 +1,11 @@
+/*
+ * Scheme 313 run-time system
+ * Exception class definitions.
+ *
+ * $Id$
+ */
+
+#define TIMER_EXCEPTION    0
+#define TYPE_EXCEPTION     1
+#define PROC_EXCEPTION     2
+#define ARG_EXCEPTION      3
