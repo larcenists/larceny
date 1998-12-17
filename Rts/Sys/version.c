abaa3efb48b43e4e03508708264ea9d5cd02ceab
lth <lth@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
1998-12-17 00:38:46 +0000

    Changed the version number to 0.40.
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@351 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Rts/Sys/version.c
+++ Rts/Sys/version.c
@@ -8,7 +8,7 @@
 #include "config.h"
 
 int  larceny_major_version = 0;
-int  larceny_minor_version = 39;
+int  larceny_minor_version = 40;
 char *larceny_version_qualifier = "";
 
 char *date = DATE;
