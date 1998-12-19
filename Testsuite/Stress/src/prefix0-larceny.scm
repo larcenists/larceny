bda013fa85e9c56467a1923ab5614f46002a9f88
lth <lth@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
1998-12-19 19:49:38 +0000

    Changed macro definitions.
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@404 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Testsuite/Stress/src/prefix0-larceny.scm
+++ Testsuite/Stress/src/prefix0-larceny.scm
@@ -1,117 +1,132 @@
-(unsafe-code #t)
-(catch-undefined-globals #f)
+; Change these as appropriate.
+
+(slow-code)
+(issue-warnings #f)
 
 ; Macros...
-; When hygienic macros are supported, get these out of src/prefix-chez.scm.
 
 ; Flonum arithmetic.
 
-(define-macro 'FLOATvector-const
-  (lambda (x)
-    (list 'quote (list->vector (cdr x)))))
-;   ((FLOATvector-const x ...) '#(x ...))))
+(define-syntax FLOATvector-const
+  (syntax-rules ()
+    ((FLOATvector-const x ...) '#(x ...))))
 
-(define-macro 'FLOATvector?
-  (lambda (x)
-    (cons 'vector? (cdr x))))
-;   ((FLOATvector? x) (vector? x))))
+(define-syntax FLOATvector?
+  (syntax-rules ()
+    ((FLOATvector? x) (vector? x))))
 
-(define-macro 'FLOATvector
-  (lambda (x)
-    (cons 'vector (cdr x))))
-;   ((FLOATvector x ...) (vector x ...))))
+(define-syntax FLOATvector
+  (syntax-rules ()
+    ((FLOATvector x ...) (vector x ...))))
 
-(define-macro 'FLOATmake-vector
-  (lambda (x)
-    (if (= (length x) 2)
-        `(make-vector ,(cadr x) 0.0)
-        (cons 'make-vector (cdr x)))))
-;   ((FLOATmake-vector n) (make-vector n 0.0))
-;   ((FLOATmake-vector n init) (make-vector n init))))
+(define-syntax FLOATmake-vector
+  (syntax-rules ()
+    ((FLOATmake-vector n) (make-vector n 0.0))
+    ((FLOATmake-vector n init) (make-vector n init))))
 
-(define-macro 'FLOATvector-ref
-  (lambda (x)
-    (cons 'vector-ref (cdr x))))
-;   ((FLOATvector-ref v i) (vector-ref v i))))
+(define-syntax FLOATvector-ref
+  (syntax-rules ()
+    ((FLOATvector-ref v i) (vector-ref v i))))
 
-(define-macro 'FLOATvector-set!
-  (lambda (x)
-    (cons 'vector-set! (cdr x))))
-;   ((FLOATvector-set! v i x) (vector-set! v i x))))
+(define-syntax FLOATvector-set!
+  (syntax-rules ()
+    ((FLOATvector-set! v i x) (vector-set! v i x))))
 
-(define-macro 'FLOATvector-length
-  (lambda (x)
-    (cons 'vector-length (cdr x))))
-;   ((FLOATvector-length v) (vector-length v))))
+(define-syntax FLOATvector-length
+  (syntax-rules ()
+    ((FLOATvector-length v) (vector-length v))))
 
-(define-macro 'nuc-const
-  (lambda (x)
-    (list 'quote (list->vector (cdr x)))))
-;   ((FLOATnuc-const x ...) '#(x ...))))
+(define-syntax nuc-const
+  (syntax-rules ()
+    ((FLOATnuc-const x ...) '#(x ...))))
 
-(define-macro 'FLOAT+
-  (lambda (x)
-    (cons '+ (cdr x))))
-;   ((FLOAT+)         0.0)
-;   ((FLOAT+ x)       x)
-;   ((FLOAT+ x y ...) (fl+ x (FLOAT+ y ...)))))
+(define-syntax FLOAT+
+  (syntax-rules ()
+    ((FLOAT+)         0.0)
+    ((FLOAT+ x)       x)
+    ((FLOAT+ x y ...) (+ x (FLOAT+ y ...)))))
 
-(define-macro 'FLOAT-
-  (lambda (x)
-    (cons '- (cdr x))))
-;   ((FLOAT- x)       (fl- x))
-;   ((FLOAT- x y ...) (fl- x (FLOAT+ y ...)))))
+(define-syntax FLOAT-
+  (syntax-rules ()
+    ((FLOAT- x)       (- x))
+    ((FLOAT- x y ...) (- x (FLOAT+ y ...)))))
 
-(define-macro 'FLOAT*
-  (lambda (x)
-    (cons '* (cdr x))))
-;   ((FLOAT*)         1.0)
-;   ((FLOAT* x)       x)
-;   ((FLOAT* x y ...) (fl* x (FLOAT* y ...)))))
+(define-syntax FLOAT*
+  (syntax-rules ()
+    ((FLOAT*)         1.0)
+    ((FLOAT* x)       x)
+    ((FLOAT* x y ...) (* x (FLOAT* y ...)))))
 
-(define-macro 'FLOAT/
-  (lambda (x)
-    (cons '/ (cdr x))))
-;   ((FLOAT/ x)       (fl/ x))
-;   ((FLOAT/ x y ...) (fl/ x (FLOAT* y ...)))))
+(define-syntax FLOAT/
+  (syntax-rules ()
+    ((FLOAT/ x)       (/ x))
+    ((FLOAT/ x y ...) (/ x (FLOAT* y ...)))))
 
-(define-macro 'FLOAT=
-  (lambda (x)
-    (cons '= (cdr x))))
-;   ((FLOAT= x y) (fl= x y))))
+(define-syntax FLOAT=
+  (syntax-rules ()
+    ((FLOAT= x y) (= x y))))
 
-(define-macro 'FLOAT<
-  (lambda (x)
-    (cons '< (cdr x))))
-;   ((FLOAT< x y) (fl< x y))))
+(define-syntax FLOAT<
+  (syntax-rules ()
+    ((FLOAT< x y) (< x y))))
 
-(define-macro 'FLOAT<=
-  (lambda (x)
-    (cons '<= (cdr x))))
-;   ((FLOAT<= x y) (fl<= x y))))
+(define-syntax FLOAT<=
+  (syntax-rules ()
+    ((FLOAT<= x y) (<= x y))))
 
-(define-macro 'FLOAT>
-  (lambda (x)
-    (cons '> (cdr x))))
-;   ((FLOAT> x y) (fl> x y))))
+(define-syntax FLOAT>
+  (syntax-rules ()
+    ((FLOAT> x y) (> x y))))
 
-(define-macro 'FLOAT>=
-  (lambda (x)
-    (cons '>= (cdr x))))
-;   ((FLOAT>= x y) (fl>= x y))))
+(define-syntax FLOAT>=
+  (syntax-rules ()
+    ((FLOAT>= x y) (>= x y))))
 
-(define-macro 'FLOATnegative?
-  (lambda (x)
-    `(< ,(cadr x) 0.0)))
-;   ((FLOATnegative? x) (fl< x 0.0))))
+(define-syntax FLOATnegative?
+  (syntax-rules ()
+    ((FLOATnegative? x) (< x 0.0))))
 
-(define-macro 'FLOATpositive?
-  (lambda (x)
-    `(> ,(cadr x) 0.0)))
-;   ((FLOATpositive? x) (fl> x 0.0))))
+(define-syntax FLOATpositive?
+  (syntax-rules ()
+    ((FLOATpositive? x) (> x 0.0))))
 
-(define-macro 'FLOATzero?
-  (lambda (x)
-    `(= ,(cadr x) 0.0)))
-;   ((FLOATzero? x) (fl= x 0.0))))
+(define-syntax FLOATzero?
+  (syntax-rules ()
+    ((FLOATzero? x) (= x 0.0))))
+
+(define-syntax FLOATabs
+  (syntax-rules ()
+    ((FLOATabs x) (abs x))))
+
+(define-syntax FLOATsin
+  (syntax-rules ()
+    ((FLOATsin x) (sin x))))
+
+(define-syntax FLOATcos
+  (syntax-rules ()
+    ((FLOATcos x) (cos x))))
+
+(define-syntax FLOATatan
+  (syntax-rules ()
+    ((FLOATatan x) (atan x))))
+
+(define-syntax FLOATsqrt
+  (syntax-rules ()
+    ((FLOATsqrt x) (sqrt x))))
+
+(define-syntax FLOATmin
+  (syntax-rules ()
+    ((FLOATmin x y) (min x y))))
+
+(define-syntax FLOATmax
+  (syntax-rules ()
+    ((FLOATmax x y) (max x y))))
+
+(define-syntax FLOATround
+  (syntax-rules ()
+    ((FLOATround x) (round x))))
+
+(define-syntax FLOATinexact->exact
+  (syntax-rules ()
+    ((FLOATinexact->exact x) (inexact->exact x))))
 
