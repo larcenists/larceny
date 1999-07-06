d9f3fd7e752e72fe1ae319553fe66dbafd9e05f7
lth <lth@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
1999-07-06 17:17:21 +0000

    Added the assert2 macro that can be enabled or disabled independently of assert.
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@655 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Rts/Sys/assert.h
+++ Rts/Sys/assert.h
@@ -2,10 +2,12 @@
  *
  * $Id$
  *
- * Larceny run-time system -- assert() macro
+ * Larceny run-time system -- assert() and assert2() macros
  */
 
 #undef assert
+#undef assert2
+
 #ifndef NDEBUG
 extern int panic_abort( const char *fmt, ... );
 #define assert( expr ) \
@@ -14,6 +16,16 @@ extern int panic_abort( const char *fmt, ... );
    : (void)panic_abort( "%s;%d: Assertion failed.", __FILE__, __LINE__ ))
 #else
 #define assert( expr ) ((void)0)
+#endif
+
+#ifndef NDEBUG2
+extern int panic_abort( const char *fmt, ... );
+#define assert2( expr ) \
+  ((expr) \
+   ? (void)0 \
+   : (void)panic_abort( "%s;%d: Assertion failed.", __FILE__, __LINE__ ))
+#else
+#define assert2( expr ) ((void)0)
 #endif
 
 /* eof */
