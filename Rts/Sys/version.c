2af5f28b334d69106430f8e19c65a72c7755d274
lth <lth@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
1999-09-24 15:48:43 +0000

    Supports a lot more operating systems!  Changed version number.
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@937 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Rts/Sys/version.c
+++ Rts/Sys/version.c
@@ -8,20 +8,33 @@
 #include "config.h"
 
 int  larceny_major_version = 0;
-int  larceny_minor_version = 43;
+int  larceny_minor_version = 45;
 char *larceny_version_qualifier = "";
 
 char *date = DATE;
 char *user = USER;
 
-#if defined(SUNOS5)
-  char *osname = "SunOS5";
-#elif defined(SUNOS4)
+/* Corresponds to the OS list in Rts/Sys/config.h */
+#if defined(SUNOS4)
   char *osname = "SunOS4";
-#elif defined(STDC_SOURCE)
-  char *osname = "Generic";
+#elif defined(SUNOS5)
+  char *osname = "SunOS5";
+#elif defined(LINUX)
+  char *osname = "Linux";
+#elif defined(BSD_UNIX)
+  char *osname = "BSD Unix";
+#elif defined(POSIX_UNIX)
+  char *osname = "Posix Unix";
+#elif defined(XOPEN_UNIX)
+  char *osname = "X/Open Unix";
+#elif defined(WIN32)
+  char *osname = "Win32";
+#elif defined(MACOS)
+  char *osname = "MacOS";
+#elif defined(GENERIC_OS)
+  char *osname = "Generic OS";
 #else
-# error "Can't defined OSNAME."
+# error "Can't define OSNAME."
 #endif
 
 /* eof */
