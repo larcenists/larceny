feb1879f6e3bdf1c43ae3787b1f94ea26cca040a
lth <lth@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
1999-09-24 15:23:10 +0000

    Added numerous fields to gc parameter structures.  Removed some redundant prototypes (gc_compute_dynamic_size, copy_object, sro) and added one (create_bdw_gc).
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@911 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Rts/Sys/gc.h
+++ Rts/Sys/gc.h
@@ -2,10 +2,7 @@
  *
  * $Id$
  *
- * Larceny -- public garbage collector interface
- *
- * The procedure create_gc() returns a new garbage collector that manages
- * some number of heap areas.
+ * Public garbage collector interface
  */
 
 #ifndef INCLUDED_GC_H
@@ -17,6 +14,7 @@
 #define MEGABYTE                     (KILOBYTE*KILOBYTE)
 
 #define MAX_HEAPS                    128               /* why not? */
+#define MAX_GENERATIONS              128               /* a better name */
 #define DEFAULT_AREAS                3
 #define DEFAULT_NURSERY_SIZE         MEGABYTE
 #define DEFAULT_STOPCOPY_SIZE        (2*MEGABYTE)
@@ -39,19 +37,29 @@ struct sc_info {		/* Any two-space copying area */
 };
 
 struct np_info {		/* Non-predictive dynamic area */
-  int steps;			/* number of steps, > 0 */
-  int stepsize;			/* size of a step in bytes, > 0 */
-  int size_bytes;               /* total size */
-  double load_factor;		/* inverse load factor */
-  int dynamic_min;              /* 0 or lower bound on expandable area */
-  int dynamic_max;		/* 0 or upper bound on expandable area */
-  double luck;			/* 0.0 .. 1.0 */
+  int    steps;			/* number of steps, > 0 */
+  int    stepsize;		/* size of a step in bytes, > 0 */
+  int    size_bytes;            /* total size */
+  int    dynamic_min;           /* 0 or lower bound on expandable area */
+  int    dynamic_max;		/* 0 or upper bound on expandable area */
+  int    extra_remset_limit;    /* 0 .. INT_MAX */
+  double load_factor;		/* Inverse load factor */
+  double luck;			/* 0.0 .. 1.0 (ought to have been 0 .. 6) */
   double phase_detection;	/* -1.0 or 0.0 .. 1.0 */
 };
 
 struct bdw_info {
   int divisor;			/* Allocation divisor */
   double load_factor;		/* inverse load factor */
+  double expansion_factor;	/* inverse expansion factor */
+  int dynamic_min;		/* 0 or lower bound on collected area */
+  int dynamic_max;		/* 0 or upper bound on collected area */
+};
+
+struct dof_info {               /* Deferred-oldest-first intermediate area */
+  int size_bytes;               /* Size of area in bytes, > 0 */
+  int steps;			/* Number of steps, > 0 */
+  double load_factor;		/* Inverse load factor */
   int dynamic_min;		/* 0 or lower bound on collected area */
   int dynamic_max;		/* 0 or upper bound on collected area */
 };
@@ -63,17 +71,20 @@ struct gc_param {		/* Parameter structure passed to create_gc() */
   bool is_stopcopy_system;
   bool use_static_area;		       /* In the nonconservative systems */
   bool use_non_predictive_collector;   /* In the generational system */
+  bool use_dof_collector;              /* In the generational system */
   bool use_incremental_bdw_collector;  /* In the conservative system */
+  bool dont_shrink_heap;	       /* In the nonconservative systems */
 
   /* Common parameters */
   word *globals;              /* globals table used by collector */
 
   /* Generational precise system */
   nursery_info_t nursery_info;
-  int ephemeral_area_count;	/* Number of ephemeral areas */
-  sc_info_t *ephemeral_info;	/* an array of these */
-  sc_info_t dynamic_sc_info;
-  np_info_t dynamic_np_info;
+  int            ephemeral_area_count;	/* Number of ephemeral areas */
+  sc_info_t      *ephemeral_info;	/* an array of these */
+  dof_info_t     dynamic_dof_info;
+  sc_info_t      dynamic_sc_info;
+  np_info_t      dynamic_np_info;
 
   /* Stop-and-copy precise system */
   sc_info_t sc_info;
@@ -86,19 +97,9 @@ struct gc_param {		/* Parameter structure passed to create_gc() */
   unsigned ssb;               /* # elements in each remset SSB */
 };
 
-
 /* In memmgr.c */
-gc_t *create_gc( gc_param_t *params, int *generations_created );
-int gc_compute_dynamic_size( int D, int S, int Q, double L, 
-			     int lower_limit, int upper_limit );
-
-
-/* In util.c */
-extern word copy_object( gc_t *gc, word obj );
-
-
-/* In sro.c */
-extern word sro( gc_t *gc, int p_tag, int h_tag, int limit );
+gc_t *create_gc( gc_param_t *params, /* OUT */ int *actual_generations );
+gc_t *create_bdw_gc( gc_param_t *params, /* OUT */ int *actual_generations );
 
 #endif
 
