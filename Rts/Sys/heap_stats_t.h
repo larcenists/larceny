13b18bf721c965302e83e5b1606e360241112845
lth <lth@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
1999-09-24 15:25:32 +0000

    Rewrite to use remset_stats_t for remset statistics.
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@914 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Rts/Sys/heap_stats_t.h
+++ Rts/Sys/heap_stats_t.h
@@ -8,12 +8,14 @@
 #ifndef INCLUDED_HEAP_STATS_T_H
 #define INCLUED_HEAP_STATS_T_H
 
+#include "remset_t.h"		/* For remset_stats_t */
+
 /* Heap statistics are returned from the collector by calling the stats()
  * member function with a generation number.  The returned structure contains
  * data from two broad categories: snapshot state and accumulated data.
  * The snapshot state says something about the dynamic state of the system:
  * the amount of memory allocated.  The accumulated data are counters that
- * are _cleared_ internally every time statistics is obtained, so at each
+ * are _cleared_ internally every time statistics are obtained, so at each
  * call the difference of the counters with respect to the previous call
  * is obtained.
  *
@@ -26,24 +28,22 @@
 
 struct heap_stats {
   /* Snapshot entries */
-  unsigned live;            /* heap bytes live (not including stack cache) */
-  unsigned stack;           /* stack bytes live, or 0 */
-  unsigned semispace1;      /* bytes allocated to semispace1 */
-  unsigned semispace2;      /* bytes allocated to semispace2, or 0 */
-  unsigned target;          /* semispace target size (policy) */
+  unsigned live;             /* heap bytes live (not including stack cache) */
+  unsigned stack;            /* stack bytes live, or 0 */
+  unsigned semispace1;       /* bytes allocated to semispace1 */
+  unsigned semispace2;       /* bytes allocated to semispace2, or 0 */
+  unsigned target;           /* semispace target size (policy) */
 
   /* "Since last call" entries */
-  unsigned copied_last_gc;  /* bytes copied during last GC */
-  unsigned moved_last_gc;   /* bytes of large objects moved during last GC */
-  unsigned frames_flushed;  /* stack frames flushed, or 0 */
-  unsigned frames_restored; /* Stack frames restored, or 0 */
-  unsigned bytes_flushed;   /* bytes of stack flushed/copied, or 0 */
-  unsigned ssb_recorded;    /* SSB entries recorded */
-  unsigned hash_recorded;   /* Hash table entries recorded */
-  unsigned hash_removed;    /* Hash table entries removed */
-  unsigned hash_scanned;    /* Hash table entries scanned */
-  unsigned words_scanned;   /* Old object space words scanned */
-  unsigned stacks_created;  /* Stacks created */
+  unsigned copied_last_gc;   /* bytes copied during last GC */
+  unsigned moved_last_gc;    /* bytes of large objects moved during last GC */
+
+  unsigned stacks_created;
+  unsigned frames_flushed;   /* stack frames flushed, or 0 */
+  unsigned frames_restored;  /* Stack frames restored, or 0 */
+  unsigned bytes_flushed;    /* bytes of stack flushed/copied, or 0 */
+
+  remset_stats_t remset_data;
 
   /* Special entries for non-predictive collector */
   unsigned np_k;            /* Number of steps */
@@ -52,11 +52,7 @@ struct heap_stats {
   unsigned np_old;          /* 1 iff NP old generation */
 
   /* "Since last call" entries */
-  unsigned np_ssb_recorded;    /* SSB entries recorded */
-  unsigned np_hash_recorded;   /* Hash table entries recorded */
-  unsigned np_hash_removed;    /* Hash table entries removed */
-  unsigned np_hash_scanned;    /* Hash table entries scanned */
-  unsigned np_words_scanned;   /* Old object space words scanned */
+  remset_stats_t np_remset_data;
 };
 
 #endif
