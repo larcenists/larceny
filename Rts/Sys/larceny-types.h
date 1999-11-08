b696565b283e8acb17cc90069972180b7480b3ec
lth <lth@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
1999-11-08 14:33:25 +0000

    Added several types for the new stats interface.
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@1133 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Rts/Sys/larceny-types.h
+++ Rts/Sys/larceny-types.h
@@ -8,6 +8,8 @@
 #ifndef INCLUDED_LARCENY_TYPES
 #define INCLUDED_LARCENY_TYPES
 
+#include "config.h"
+
 /* Scalar data types. */
 typedef unsigned word;
 typedef int s_word;
@@ -38,9 +40,8 @@ typedef struct old_heap old_heap_t;
 /* static_heap_t is elaborated in static_heap_t.h */
 typedef struct static_heap static_heap_t;
 
-/* remset_t and remset_stats_t are elaborated in remset_t.h */
+/* remset_t is elaborated in remset_t.h */
 typedef struct remset remset_t;
-typedef struct remset_stats remset_stats_t;
 
 /* semispace_t and ss_chunk_t are elaborated in semispace_t.h */
 typedef struct semispace semispace_t;
@@ -49,6 +50,16 @@ typedef struct ss_chunk ss_chunk_t;
 /* heapio_t and metadata_block_t are elaborated in heapio.h */
 typedef struct heapio_t heapio_t;
 typedef struct metadata_block_t metadata_block_t;
+
+/* stats.h */
+typedef struct gclib_stats gclib_stats_t;
+typedef struct gc_stats gc_stats_t;
+typedef struct gen_stats gen_stats_t;
+typedef struct remset_stats remset_stats_t;
+typedef struct stack_stats stack_stats_t;
+#if defined(SIMULATE_NEW_BARRIER)
+typedef struct swb_stats swb_stats_t;
+#endif
 
 /* Currently these are in gc.h but should perhaps move? */
 typedef struct np_info np_info_t;
@@ -59,14 +70,7 @@ typedef struct nursery_info nursery_info_t;
 typedef struct gc_param gc_param_t;
 typedef struct old_param old_param_t;
 
-/* heap_stats_t.h */
-typedef struct heap_stats heap_stats_t;
-typedef struct dof_stats dof_stats_t;
-
 /* los_t.h */
 typedef struct los los_t;
-
-/* gclib.h */
-typedef struct gclib_stats gclib_stats_t;
 
 #endif /* INCLUDED_LARCENY_TYPES */
