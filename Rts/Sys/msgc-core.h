d0f392a3c9a51ee6ea965e0c69cea43aacc8c84b
lth <lth@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2000-03-06 21:36:21 +0000

    Added msgc_mark_range, msgc_mark_object, msgc_push_constituents, and msgc_assert_conservative_approximation.
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@1390 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Rts/Sys/msgc-core.h
+++ Rts/Sys/msgc-core.h
@@ -12,16 +12,35 @@ typedef struct msgc_context msgc_context_t;
 extern msgc_context_t *msgc_begin( gc_t *gc );
   /* Create a mark-sweep GC context for the given collector and
      return the context.
+
+     No heap memory may be allocated between the call to msgc_begin()
+     and the call to msgc_mark_objects_from_roots() because the 
+     former computes the heap size and allocates data structures 
+     for marking.
      */
 
 extern void msgc_mark_objects_from_roots( msgc_context_t *context, 
                                           int *marked, int *traced,
                                           int *words_marked );
   /* Mark the entire heap from roots, marking only objects (not interior
-     locations).  Return number of objects marked in *marked, number of
-     words marked in *words_marked, and number of pointers traced in *traced.
+     locations).  Adds the number of objects marked to *marked, number of
+     words marked to *words_marked, and number of pointers traced to *traced.
      */
 
+extern void msgc_mark_range( msgc_context_t *context, void *bot, void *lim );
+  /* Mark all words in the range [bot,lim) as reachable.
+     */
+
+extern void msgc_mark_object( msgc_context_t *context, word obj );
+  /* Mark the object referenced by the tagged _or untagged_ pointer OBJ.
+     */
+
+extern void msgc_push_constituents( msgc_context_t *context, word obj );
+  /* OBJ must be a taged pointer to an object in the heap.  All
+     pointer members of the objects are pushed on the mark stack, 
+     resulting in them being used as roots for the next mark phase.
+     */
+     
 extern bool msgc_object_marked_p( msgc_context_t *context, word obj );
   /* OBJ must be a tagged pointer to an object (note: _not_ to an
      arbitrary address) in the heap.  Returns TRUE iff the object
@@ -30,6 +49,14 @@ extern bool msgc_object_marked_p( msgc_context_t *context, word obj );
      
 extern void msgc_end( msgc_context_t *context );
   /* Free the context data structure and any resources it uses.
+     */
+
+extern void msgc_assert_conservative_approximation( msgc_context_t *context );
+  /* Check that the mark bitmap in the context is a conservative
+     approximation of the actual reachability-based bitmap, and
+     trigger an assertion if not.
+     
+     This operation is not cheap.
      */
 
 /* eof */
