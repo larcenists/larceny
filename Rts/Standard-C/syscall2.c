372c4211b9b43f4051846012e69aa40cfbde4966
lth <lth@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2004-08-02 21:26:05 +0000

    Bug fixes and other support for Win32
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@2073 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Rts/Standard-C/syscall2.c
+++ Rts/Standard-C/syscall2.c
@@ -12,19 +12,8 @@ extern cont_t *twobit_load_table[];
 
 void larceny_segment_code_address( word w_id, word w_number )
 {
-  word x = (word)twobit_load_table[nativeuint(w_id)][nativeuint(w_number)];
-
-#if 0
-  if (is_fixnum( x ))
-    globals[ G_RESULT ] = x;
-  else
-    panic_abort( "Table lookup failed in segment_code_address: unaligned code pointer!" );
-#else
-  /* It actually does not matter if the pointer is not aligned, since
-     the GC does not inspect the object pointed to -- it is outside the
-     heap. */
-  globals[ G_RESULT ] = x;
-#endif
+  globals[G_RESULT] = 
+      ENCODE_CODEPTR(twobit_load_table[nativeuint(w_id)][nativeuint(w_number)]);
 }
 
 /* eof */
