45a9c92e8fd04b4c8fb51aafd49c9af61954a9e8
tov <tov@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2006-03-18 18:45:01 +0000

    tail call convention works
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@2790 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- Asm/Intel/Experiments/tailcall.scm
+++ Asm/Intel/Experiments/tailcall.scm
@@ -1,170 +1,120 @@
 ;;
 ;; This is designed to be called from a custom loader :-p
+;; Calling convention:
+;;   Arguments in EAX, EBX, ECX, EDX, ESI, EDI
+;;   Tail call with jmp; non-tail call with funcall
+;;   Return with funret
+;;   Use ESP at your leisure
 ;;
 (sassy-make-bin "test.i"
                 (sassy
-                  '((entry start)
-                    (macro enter
-                           (begin (push ebp)
-                                  (mov ebp esp)))
-                    (macro leave
-                           (begin (mov esp ebp)
-                                  (pop ebp)))
-                    (macro defun
-                           (lambda (name . body)
-                             `(label ,name
-                                     enter
-                                     ,@body
-                                     leave
-                                     (ret))))
-
-                    (macro save
+                  '((macro save
                            (lambda (size)
                              `(sub esp ,(* 4 size))))
                     (macro unsave
                            (lambda (size)
                              `(add esp ,(* 4 size))))
-                    (macro arg
-                           (lambda (n)
-                             `(& ebp ,(* 4 (+ 2 n)))))
                     (macro loc
                            (lambda (n)
                              `(& esp ,(* 4 n))))
-
+                    (macro funcall
+                           (lambda (f)
+                             `(begin
+                                (push ebp)
+                                (mov ebp esp)
+                                (sub ebp 4)
+                                (call ,f)
+                                (pop ebp)
+                                )))
+                    (macro funret
+                           (lambda ()
+                             '(begin
+                                (mov esp ebp)
+                                (ret))))
                     (macro syscall
-                           (lambda (name . tail)
-                             (let ((params
-                                     (case name
-                                       ((exit)      '(1 1))
-                                       ((fork)      '(2 0))
-                                       ((read)      '(3 3))
-                                       ((write)     '(4 3))
-                                       ((open)      '(5 3))
-                                       ((close)     '(6 1))
-                                       ((waitpid)   '(7 3))
-                                       ((creat)     '(8 2))
-                                       ((link)      '(9 2))
-                                       ((unlink)   '(10 1))
-                                       ((execve)   '(11 1))
-                                       ((chdir)    '(12 1))
-                                       ((time)     '(14 1))
-                                       (else (error "unknown syscall")))))
-                               `(begin
-                                  (push ,(car params))
-                                  (push ,(cadr params))
-                                  ,@(if (null? tail)
-                                     `((call _syscall))
-                                     `((push ,(car tail))
-                                       (jmp _syscall)))))))
+                           (lambda (which)
+                             `(begin
+                                (mov eax ,which)
+                                (int #x80))))
+                    (macro c-arg
+                           (lambda (n)
+                             `(& ebp ,(* 4 (+ 2 n)))))
+
+                    (macro sys_exit      1)
+                    (macro sys_fork      2)
+                    (macro sys_read      3)
+                    (macro sys_write     4)
+                    (macro sys_open      5)
+                    (macro sys_close     6)
+                    (macro sys_waitpid   7)
+                    (macro sys_creat     8)
+                    (macro sys_link      9)
+                    (macro sys_unlink   10)
+                    (macro sys_execve   11)
+                    (macro sys_chdir    12)
+                    (macro sys_time     14)
 
                     (data)
 
                     (text
-                      (defun main
+                      (label main
+                        (push ebp)
+                        (mov ebp esp)
+
                         (push ebx)
                         (push esi)
                         (push edi)
 
-                        (save 2)
-
-                        (push #\a)
-                        (call putchar)
-
-                        (mov ecx (arg 1))
-                        (mov ecx (& ecx))
-
-                        (xor eax eax)
-                        (mov al (& ecx))
-                        (push eax)
-                        (call putchar)
-
-
-                        (mov al (& 1 ecx))
-                        (push eax)
-                        (call putchar)
-
-                        (push ecx)
-                        (call putstr)
-
-                        (push 0)
-                        (syscall exit)
+                        (mov ecx (c-arg 1))
 
                         (leap
                           (iter
                             (begin
-                              (mov (loc 1) ecx)
-                              (mov (loc 0) eax)
-                              (call putstr)
-                              (push #\space)
-                              (call putchar)
-                              (mov ecx (loc 1))
+                              (push ecx)
+                              (funcall putstr)
+                              (mov eax #\space)
+                              (funcall putchar)
+                              (pop ecx)
                               (mark
                                 (add ecx 4))
                               (mov eax (& ecx))
                               (!= eax 0))))
 
-                        (push #\newline)
-                        (call putchar)
-
-                        (unsave 2)
+                        (mov eax #\newline)
+                        (funcall putchar)
 
                         (pop edi)
                         (pop esi)
                         (pop ebx)
-                        (xor eax eax))
 
-                      (label _syscall
-                        (push ebp)
-                        (mov ebp esp)
-                        (mov eax (& 8 ebp)) ; number of arguments
-                        (add esp 16)
-                        (seq
-                          (>= eax 1) 
-                          (pop ebx)
-                          (>= eax 2)
-                          (pop ecx)
-                          (>= eax 3)
-                          (pop edx)
-                          (>= eax 4)
-                          (pop esi)
-                          (>= eax 5)
-                          (pop edi))
-                        (mov eax (& 12 ebp)) ; first argument
-                        (int #x80)
-                        (mov ebx (& 4 ebp))
-                        (mov ebp (& ebp))
-                        (push ebx)
+                        (xor eax eax)
+
+                        (mov esp ebp)
+                        (pop ebp)
                         (ret))
 
                       (label putstr
-                        (push ebp)
-                        (mov ebp esp)
-                        (mov ecx (arg 0))
+                        (mov ecx eax)
                         (leap
                           (iter
                             (begin
                               (push ecx)
-                              (push eax)
-                              (call putchar)
+                              (funcall putchar)
                               (pop ecx)
                               (inc ecx)
                               (mark
                                 (xor eax eax))
                               (mov al (& ecx))
                               (!= al 0))))
-                        (mov esp ebp)
-                        (pop ebp)
-                        (ret))
+                        (funret))
                       
                       ;; putchar is a dynamic leaf procedure
                       (label putchar
-                        (pop edx)
-                        (mov eax esp)
-                        (add esp 4)
-                        (push 1)
                         (push eax)
-                        (push 0)
-                        (syscall write edx)
-
+                        (xor ebx ebx)
+                        (mov ecx esp)
+                        (mov edx 1)
+                        (syscall sys_write)
+                        (funret)
                         )))))
 
