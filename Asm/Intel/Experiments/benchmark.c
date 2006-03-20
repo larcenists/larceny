9e9ebabf219ad3b603895c09fe158826ddff7370
tov <tov@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2006-03-20 22:56:38 +0000

    runs, does not crash
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@2791 c9681fa6-b8ae-40c0-a6da-707a35ad5973

---
+++ Asm/Intel/Experiments/benchmark.c
@@ -0,0 +1,185 @@
+#include <sys/types.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+
+struct TreeNode;
+typedef struct TreeNode TN;
+
+typedef int (*nextFun_t)(TN *, int, int);
+
+struct TreeNode {
+    int key;
+    TN *left;
+    TN *right;
+    TN *nextTree;
+    int nextKey;
+    nextFun_t nextFun;
+    int dummy;
+};
+
+void tree_init(TN *thisTree, size_t size)
+{
+    int key = 0, index;
+    TN *left = NULL, *right = NULL;
+
+    bzero(thisTree, size * sizeof(TN));
+
+    for (index = size - 1; index >= 0; --index) {
+        thisTree[index].key   = key;
+        thisTree[index].left  = left;
+        thisTree[index].right = right;
+
+        if (key > 0) {
+            left  = NULL;
+            right = &thisTree[index];
+            key   = -key - 1;
+        } else {
+            left  = &thisTree[index];
+            right = NULL;
+            key   = -key + 1;
+        }
+    }
+}
+
+TN *tree_find(TN *tree, int key)
+{
+    while (tree->key != key) {
+        tree = key < tree->key ? tree->left : tree->right;
+    }
+
+    return tree;
+}
+
+void tree_connect(TN *src, TN *dest, nextFun_t fun)
+{
+    src = tree_find(src, 0);
+
+    src->nextTree = dest;
+    src->nextFun  = fun;
+    src->nextKey  = 0;
+}
+
+size_t file_size(const char *filename)
+{
+    long rv;
+    FILE *f = fopen(filename, "r");
+
+    assert(f);
+
+    rv = fseek(f, 0, SEEK_END);
+    assert(rv == 0);
+
+    rv = ftell(f);
+    assert(rv != -1);
+
+    fclose(f);
+
+    return (size_t)rv;
+}
+
+char *file_read(const char *filename)
+{
+    size_t rv, size;
+    FILE *f;
+    char *buf;
+
+    size = file_size(filename);
+    assert(size);
+
+    buf = malloc(size);
+    assert(buf);
+
+    f = fopen(filename, "r");
+    assert(f);
+
+    rv = fread(buf, 1, size, f);
+    assert(rv == size);
+
+    fclose(f);
+
+    return buf;
+}
+
+static inline float timing()
+{
+    static double time1 = 1e35;
+    double time2, tmp;
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    time2 = tv.tv_sec + 1e-6 * tv.tv_usec;
+    tmp   = time2 - time1;
+    time1 = time2;
+
+    return tmp;
+}
+
+int main(int argc, char **argv)
+{
+    unsigned int bs, ts, nb, ni;
+    unsigned int i;
+
+    char *data;
+    TN *tree;
+    nextFun_t entry;
+
+    size_t bskip, pskip;
+
+    const char *const program = "benchmark.i";
+    size_t progsize;
+    char *progtext;
+
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s BLOCKSIZE TREESIZE NBLOCKS ITERS\n",
+                argv[0]);
+        exit(1);
+    }
+
+    progsize = file_size(program);
+    progtext = file_read(program);
+
+    bs = atoi(argv[1]);
+    ts = atoi(argv[2]);
+    nb = atoi(argv[3]);
+    ni = atoi(argv[4]);
+
+    if (bs == 0) {
+        bskip = progsize + ts * sizeof(TN);
+        pskip = progsize;
+    } else {
+        assert(bs >= progsize);
+        assert(bs >= ts * sizeof(TN));
+
+        bskip = 2 * bs;
+        pskip = bs;
+    }
+
+    data = malloc(nb * bskip);
+
+    for (i = 0; i < nb; ++i) {
+        nextFun_t f0 = (nextFun_t)(data + i * bskip);
+        TN *t0       = (TN *)(data + i * bskip + pskip);
+
+        nextFun_t f1 = (nextFun_t)(data + ((i + 1) % nb) * bskip);
+        TN *t1       = (TN *)(data + ((i + 1) % nb) * bskip + pskip);
+
+        // Copy program text:
+        memcpy(f0, progtext, progsize);
+
+        // Create a tree:
+        tree_init(t0, ts);
+
+        // Link this tree to the next tree and next code:
+        tree_connect(t0, t1, f1);
+    }
+
+    tree  = (TN *)(data + pskip);
+    entry = (nextFun_t)data;
+
+    entry(tree, 0, ni);
+
+    return 0;
+}
---
+++ Asm/Intel/Experiments/benchmark.scm
@@ -0,0 +1,85 @@
+;; This is intended to benchmark certain effects of code layout.
+;; Something like this:
+;;
+;; struct TreeNode {
+;;     int key;
+;;     struct TreeNode *left;
+;;     struct TreeNode *right;
+;;     struct TreeNode *nextTree;
+;;     int nextKey;
+;;     int (*nextFun)(struct TreeNode *, int, int);
+;;     int dummy;
+;; };
+;;
+;; main(struct TreeNode *tree, int key, count) {
+;;     while (tree->key != key)
+;;         tree = key < tree->key ? tree->left : tree->right;
+;;
+;;     if (count == 0)
+;;         return tree->nextKey;
+;;     else
+;;         return tree->nextFun(tree->nextTree, tree->nextKey, count - 1);
+;; }
+;;
+;; This is designed to be called from benchmark.c or winbenchmark.c.
+;;
+;; C calling convention sort of, but with sneaky tail calls.
+;;
+(sassy-make-bin "benchmark.i"
+  (sassy
+    '((macro c-arg
+             (lambda (n)
+               `(& esp ,(* 4 (+ 1 n)))))
+
+      (macro t:key       0)
+      (macro t:left      4)
+      (macro t:right     8)
+      (macro t:nextTree 12)
+      (macro t:nextKey  16)
+      (macro t:nextFun  20)
+      (macro t:dummy    24)
+
+      (text
+        (label main
+          (mov eax (c-arg 1))         ; eax := key
+          (mov ecx (c-arg 0))         ; ecx := tree
+
+          (while
+            (begin
+              (mov edx (& ecx t:key))
+              (!= eax edx))
+            (if (< eax edx)
+              (mov ecx (& ecx t:left))
+              (mov ecx (& ecx t:right))))
+
+          ;; Should expand to something like:
+;          (locals (loop left break)
+;            (label loop)              ; loop:
+;            (mov edx (& ecx t:key))   ; edx := tree->key
+;            (cmp eax edx)             ; if (key == tree->key)
+;            (je break)                ;   break
+;            (jl left)                 ; if (key < tree->key) goto left;
+;            (mov ecx (& ecx t:right)) ; tree := tree->right
+;            (jmp loop)                ; goto loop
+;            (label left)              ; left:
+;            (mov ecx (& ecx t:left))  ; tree := tree->left
+;            (jmp loop)                ; goto loop
+;            (label break))
+
+          (mov eax (c-arg 2))
+          (mov (& ecx t:dummy) eax)
+
+          (if
+            (= eax 0)
+            (begin
+              (mov eax (& ecx t:nextKey))
+              (ret))
+            (begin
+              (dec eax)
+              (mov (c-arg 2) eax)
+              (mov eax (& ecx t:nextKey))
+              (mov (c-arg 1) eax)
+              (mov eax (& ecx t:nextTree))
+              (mov (c-arg 0) eax)
+              (jmp (& ecx t:nextFun)))))))))
+
