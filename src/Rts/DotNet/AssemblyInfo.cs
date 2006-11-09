d25e0e1e177b7bf763d1608f8ae32e7ef9eefb45
cburns <cburns@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2006-11-09 01:56:15 +0000

    Adding AssemblyInfo.cs to the Scheme.dll build so that we can give it a version number. The version number is hard-wired into the file as a quick fix for 0.93 release, but there should be a way to include it automatically from src/Rts/Sys/version.c
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@3763 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- src/Rts/DotNet/AssemblyInfo.cs
+++ src/Rts/DotNet/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Security.Permissions;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PLT")]
 [assembly: AssemblyProduct("Common Larceny")]
-[assembly: AssemblyCopyright("2004")]
+[assembly: AssemblyCopyright("2006")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,7 +27,7 @@ using System.Security.Permissions;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.93.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
