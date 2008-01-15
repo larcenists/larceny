becb409db25cffe9e850ad71cca014799173bdcf
pnkfelix <pnkfelix@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2008-01-15 21:49:24 +0000

    When we're being noisy, lets unconditionally emit the message.
    (Being noisy is almost always conditionalized, and this function is
    only for debugging purposes anyway, so I think this is reasonable.)
    
    
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@5321 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- src/Rts/Sys/semispace.c
+++ src/Rts/Sys/semispace.c
@@ -401,10 +401,10 @@ bool ss_is_address_mapped( semispace_t *ss, word *addr, bool noisy )
   bool ret = FALSE;
   for (i=0; i < ss->n; i++ ) {
     if (noisy) 
-      annoyingmsg( "ss_is_address_mapped ss: 0x%08x addr: 0x%08x i: %d"
-                   " bot: 0x%08x top: 0x%08x lim: 0x%08x",
-                   ss, addr, i,
-                   ss->chunks[i].bot, ss->chunks[i].top, ss->chunks[i].lim );
+      consolemsg( "ss_is_address_mapped ss: 0x%08x addr: 0x%08x i: %d"
+                  " bot: 0x%08x top: 0x%08x lim: 0x%08x",
+                  ss, addr, i,
+                  ss->chunks[i].bot, ss->chunks[i].top, ss->chunks[i].lim );
     if ((ss->chunks[i].bot <= addr) && (addr < ss->chunks[i].top)) {
       assert( !ret ); ret = TRUE;
     }
