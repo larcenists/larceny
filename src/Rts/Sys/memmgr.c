d2f0a2541872b4f78e4e802d5dde88d4d31ed953
pnkfelix <pnkfelix@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2008-01-30 19:08:31 +0000

    There was a serious oversight in the implementation I had for {{{find_space_rgnl}}}; the {{{gc_allocated_to_areas}}} method does not return correct results in the midst of collection.
    In particular, it would use the number of allocated bytes at the
    ''start'' of collection.  Thus it would claim that that region
    associated with the first tospace was filled up when in fact we had
    only filled up a single chunk of a semispace; it would then create a
    fresh space, and then subsequent invocations of {{{find_space_rgnl}}}
    would claim that the fresh space has nothing allocated to it, even
    though the collector had been forwarding objects into it.  (Again,
    this is because it would use the number of allocated bytes at the
    ''start'' of collection). 
    
    So we would end up with a region gradually growing larger than the
    bounds we are trying to impose on them, and thus the pause time would
    not be bounded.
    
    This change is an attempt to fix this, by doing the same calculation
    that is used to set the {{{allocated}}} field of an {{{old-heap}}}
    structure to compute the value of {{{cur_allocated}}} for the region.
    
    This fixes the bad pause times I was seeing on the perm9 benchmark.  I
    still need to evaluate how other benchmarks are doing...
    
    
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@5355 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- src/Rts/Sys/memmgr.c
+++ src/Rts/Sys/memmgr.c
@@ -1663,10 +1663,13 @@ static semispace_t *find_space_rgnl( gc_t *gc, unsigned bytes_needed,
 				     semispace_t *current_space,
 				     semispace_t **spaces_filtered, int sf_len )
 {
-  int cur_allocated = 
-    gc_allocated_to_areas( gc, gset_singleton( current_space->gen_no ));
+  int cur_allocated;
   int max_allocated = 
     gc_maximum_allotted( gc, gset_singleton( current_space->gen_no ));
+
+  ss_sync( current_space );
+  cur_allocated = 
+    current_space->allocated+los_bytes_used( gc->los, current_space->gen_no );
   if (cur_allocated + bytes_needed < max_allocated ) {
     ss_expand( current_space, max( bytes_needed, GC_CHUNK_SIZE ));
     return current_space;
