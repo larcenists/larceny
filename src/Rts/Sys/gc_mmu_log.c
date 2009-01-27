ef05bfa3a28eb3a3a58da359f329de56185a5cd4
pnkfelix <pnkfelix@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2009-01-27 17:15:34 +0000

    Checkpointing work so far on gathering MMU data.
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@5981 c9681fa6-b8ae-40c0-a6da-707a35ad5973

---
+++ src/Rts/Sys/gc_mmu_log.c
@@ -0,0 +1,285 @@
+/* Copyright 2009 Felix S Klock II.              -*- indent-tabs-mode: nil -*-
+ *
+ * $Id$
+ *
+ * Larceny run-time system -- event log providing minimum mutator
+ * utilization (MMU) data
+ */
+
+#include "larceny.h"
+#include "gc_mmu_log.h"
+
+#define EXPORT
+
+struct event_phase_stats {
+  unsigned current_real;
+  unsigned current_cpu;
+  unsigned min_real;
+  unsigned min_cpu;
+  unsigned max_real;
+  unsigned max_cpu;
+};
+
+struct ref_to_window_start {
+  int buf_idx;
+  int entry_offset;
+  /* Represents back pointers to the start of a window in the log
+   * buffer.  (As new events are enqueued, the window shifts forward.)
+   * The window *conceptually* starts at 
+   *    STARTTIME(log->buffer.entries[buf_idx]) + entry_offset
+   * 
+   * If buf_idx is negative, then not enough phase events have been
+   * enqueued to fill up the window.  In this case, the window starts
+   * at STARTTIME(log->buf_first); log->buf_first should be 0 in any
+   * case where this situation arises).
+   * In this case, the entry_offset is *not* an offset in some
+   * particular entry, but rather sums of the total lengths of the
+   * events accumulated so far.
+   *
+   * When no references remain to an entry in the buffer, it can be
+   * "dequeued" by shifting the buffer start pointer forward.
+   */
+};
+
+struct event_window { 
+  int size; /* size of the window in milliseconds */ 
+
+  /* invariant: one entry for each gc_log_phase_t */
+  struct event_phase_stats mutator;
+  struct event_phase_stats misc_memmgr;
+  struct event_phase_stats minorgc;
+  struct event_phase_stats majorgc;
+  struct event_phase_stats summarize;
+  struct event_phase_stats smircy;
+
+  struct ref_to_window_start window_start_real;
+  struct ref_to_window_start window_start_cpu;
+};
+
+struct log_entry {
+  gc_log_phase_t phase;
+  unsigned elapsed_real;
+  unsigned elapsed_cpu;
+};
+
+struct gc_mmu_log {
+  struct {
+    struct log_entry *entries;
+    int capacity;
+    int first;
+    int end;
+  } buffer;
+  /* buffer is made up of entries: 
+   *   [ entries[i] | f <= i < min(e,k) ] ++ [ buf[i] | 0 <= i < f ]
+   * where 
+   *   f = first 
+   *   e = last 
+   *   k = capacity
+   */
+
+  struct {
+    gc_log_phase_t phase;
+    unsigned start_real;
+    unsigned start_cpu;
+  } in_progress;
+
+  struct {
+    struct event_window *array;
+    int len;
+  } windows;
+};
+
+static void clear_phase_stats( struct event_phase_stats *s )
+{
+  s->current_real = 0;
+  s->current_cpu  = 0;
+  s->min_real     = 0;
+  s->min_cpu      = 0;
+  s->max_real     = 0;
+  s->max_cpu      = 0;
+}
+
+static void clear_event_window( struct event_window *e )
+{
+  clear_phase_stats( &e->mutator );
+  clear_phase_stats( &e->misc_memmgr );
+  clear_phase_stats( &e->minorgc );
+  clear_phase_stats( &e->majorgc );
+  clear_phase_stats( &e->summarize );
+  clear_phase_stats( &e->smircy );
+
+  e->window_start_real.buf_idx = -1;
+  e->window_start_real.entry_offset = 0;
+  e->window_start_cpu.buf_idx = -1;
+  e->window_start_cpu.entry_offset = 0;
+}
+
+EXPORT gc_mmu_log_t *
+create_gc_mmu_log( int *window_lens, int buffer_length, gc_log_phase_t init )
+{
+  gc_mmu_log_t *log;
+
+  log = (gc_mmu_log_t*)must_malloc( sizeof( gc_mmu_log_t ));
+
+  log->buffer.entries = (struct log_entry*)
+    must_malloc( buffer_length*sizeof( struct log_entry ));
+  log->buffer.capacity = buffer_length;
+  log->buffer.first = 0;
+  log->buffer.end = 0;
+
+  log->in_progress.phase = init;
+  log->in_progress.start_real = osdep_realclock();
+  log->in_progress.start_cpu  = osdep_cpuclock();
+
+  { 
+    int i;
+    i = 0;
+    while ( window_lens[i] != -1 ) {
+      i++;
+    }
+    log->windows.len = i;
+    log->windows.array = (struct event_window*)
+      must_malloc( log->windows.len*sizeof( struct event_window ));
+    for ( i = 0; i < log->windows.len; i++ ) {
+      clear_event_window( &log->windows.array[i] );
+      log->windows.array[i].size = window_lens[i];
+    }
+  }
+
+  return log;
+}
+
+#define ENTRY_ELAPSED( isreal, entry ) \
+  ((isreal)?(entry).elapsed_real:(entry).elapsed_cpu)
+
+static void update_ref_to_start( gc_mmu_log_t *log,
+                                 struct event_window *w, 
+                                 struct ref_to_window_start *r,
+                                 unsigned new_elapsed,
+                                 bool isreal_and_not_cpu ) 
+{
+  int size = w->size;
+  int unshifted_plus_new;
+  if (r->buf_idx < 0) {
+    /* was still waiting for sufficient events to fill window ... */
+    unshifted_plus_new = r->entry_offset + new_elapsed;
+    if (unshifted_plus_new < size) {
+      /* ... and we're still waiting. */
+      r->entry_offset += new_elapsed;
+      return;
+    } else {
+      /* now we have sufficient events to fill window. */
+      assert( log->buffer.first == 0 );
+    }
+  } else {
+    /* (increase the window in the other direction so that it starts
+     *  at the beginning of the first entry; see comment below.) */
+    unshifted_plus_new = size + new_elapsed + r->entry_offset;
+  }
+
+  /* At this point, the conceptual window starts at the beginning of
+   * the entry at log->buffer.first and we've enqueued an entry that
+   * overflows the window; so we need to adjust the window
+   * accordingly. */
+
+  assert( unshifted_plus_new >= size );
+
+  /* Step through log until we can go no further. */
+  { 
+    int i, shifted_plus_new, elapsed;
+    i = log->buffer.first;
+    shifted_plus_new = unshifted_plus_new;
+    elapsed = ENTRY_ELAPSED( isreal_and_not_cpu, log->buffer.entries[i] );
+    while ((shifted_plus_new - elapsed) > size) {
+      shifted_plus_new -= elapsed;
+      i = ((i+1) % log->buffer.capacity);
+      assert( i != log->buffer.end );
+      elapsed = ENTRY_ELAPSED(isreal_and_not_cpu, log->buffer.entries[i]);
+    }
+    assert( elapsed == ENTRY_ELAPSED( isreal_and_not_cpu, log->buffer.entries[i] ));
+    assert( (shifted_plus_new - elapsed) <= size );
+    assert( shifted_plus_new >= size );
+
+    r->buf_idx = i;
+    r->entry_offset = size - (shifted_plus_new - elapsed);
+  }
+}
+
+/* shifts the buf_idx and buf_entry_offsets forward to reflect the
+ * enqueuing of the incoming event.
+ * (Note that this may require stepping through an arbitrary number of
+ *  events in the log.)
+ */
+static void update_window( gc_mmu_log_t *log,
+                           struct event_window *w, 
+                           gc_log_phase_t incoming, 
+                           unsigned elapsed_real,
+                           unsigned elapsed_cpu )
+{
+  update_ref_to_start( log, w, &w->window_start_real, elapsed_real, TRUE );
+  update_ref_to_start( log, w, &w->window_start_cpu,  elapsed_cpu, FALSE );
+}
+static void update_windows( gc_mmu_log_t *log, 
+                            gc_log_phase_t incoming, 
+                            unsigned elapsed_real,
+                            unsigned elapsed_cpu )
+{
+  struct event_window *w;
+  int i;
+  for ( i = 0; i < log->windows.len; i++ ) {
+    w = &log->windows.array[i];
+    update_window( log, w, incoming, elapsed_real, elapsed_cpu );
+  }
+}
+
+static void enqueue( gc_mmu_log_t *log,
+                     gc_log_phase_t incoming,
+                     unsigned elapsed_real,
+                     unsigned elapsed_cpu )
+{
+  int enq = log->buffer.end;
+
+  update_windows( log, incoming, elapsed_real, elapsed_cpu );
+
+  log->buffer.entries[enq].phase = incoming;
+  log->buffer.entries[enq].elapsed_real = elapsed_real;
+  log->buffer.entries[enq].elapsed_cpu  = elapsed_cpu;
+
+  enq = (enq+1) % log->buffer.capacity;
+  assert( enq != log->buffer.first );
+  log->buffer.end = enq;
+}
+
+EXPORT
+void gc_mmu_log_phase_shift( gc_mmu_log_t *log, 
+                             gc_log_phase_t prev, gc_log_phase_t next ) 
+{
+  unsigned finis_real = osdep_realclock();
+  unsigned finis_cpu  = osdep_cpuclock();
+  unsigned start_real, start_cpu;
+  unsigned elapsed_real, elapsed_cpu;
+
+  assert2( log->in_progress.phase == prev );
+  assert2( prev != next );
+
+  start_real = log->in_progress.start_real;
+  start_cpu  = log->in_progress.start_cpu;
+
+  elapsed_real = finis_real - start_real;
+  elapsed_cpu  = finis_cpu  - start_cpu;
+
+  enqueue( log, prev, elapsed_real, elapsed_cpu );
+
+  start_real = osdep_realclock();
+  start_cpu  = osdep_cpuclock();
+
+  log->in_progress.phase = next;
+  log->in_progress.start_real = start_real;
+  log->in_progress.start_cpu  = start_cpu;
+}
+
+EXPORT
+void gc_mmu_log_print_data( gc_mmu_log_t *log )
+{
+  assert(0);
+}
---
+++ src/Rts/Sys/gc_mmu_log.h
@@ -0,0 +1,47 @@
+/* Copyright 2009 Felix S Klock II.              -*- indent-tabs-mode: nil -*-
+ *
+ * $Id$
+ *
+ * Larceny run-time system -- event log providing minimum mutator
+ * utilization (MMU) data
+ */
+
+typedef enum {
+  gc_log_phase_mutator, 
+  gc_log_phase_misc_memmgr, gc_log_phase_minorgc, gc_log_phase_majorgc, 
+  gc_log_phase_summarize, 
+  gc_log_phase_smircy
+} gc_log_phase_t;
+
+gc_mmu_log_t *
+create_gc_mmu_log( int *window_lens, int buffer_length, gc_log_phase_t init );
+  /* Creates an event log accumulating N windows, where N = the length
+   * of the longest prefix of positive entries in window_lens.
+   * 
+   * The i'th window tracks shifts over a window of size W_i milliseconds, 
+   * where W_i = windows_lengths[i].  The log is backed by an internal
+   * circular buffer of length L, where L = buffer_length.
+   * 
+   * If phase shifts happen so rapidly that the internal buffer
+   * becomes exhausted, then the phase shift method is allowed to die
+   * (loudly), drop the lost events (silently), or drop only the
+   * windows which are too large to continue maintaining.  Thus this
+   * interface is not terribly robust, but it should serve well enough
+   * for a short term use.
+   *
+   * After this constructor returns, it will not retain a reference to
+   * the window_lengths array.
+   */
+
+void gc_mmu_log_phase_shift( gc_mmu_log_t *log, 
+                             gc_log_phase_t prev, gc_log_phase_t next );
+  /* requires: prev is phase of last log entry and prev != next
+   * 
+   * effects: signals a control shift from one phase of runtime to another.
+   *
+   * (client provides prev so that we can sanity check the phase shifts) 
+   */
+
+void gc_mmu_log_print_data( gc_mmu_log_t *log );
+  /* prints current MMU log data to the console.
+   */
