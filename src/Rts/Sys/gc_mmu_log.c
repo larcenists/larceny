26781559ffa009c784cde0dc0f804de29dd60cb1
pnkfelix <pnkfelix@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2009-01-28 03:52:39 +0000

    1. Added {{{CHECK_REP}}} macro and a (partial) implementation.  (Used  for tracking down internal bugs in the buffer and window maintenance)
     2. Changed representation of fields in {{{event_phase_stats}}} from
     {{{unsigned}}} to {{{int}}} so that I will be semi-likely to notice
     overflow.  (should I make them {{{long long}}}...?)
    
     3. Fixed {{{min_real}}} and {{{min_cpu}}} initialization so that they
     start at (an approximation of) infinity.
    
      4. Alpha-renamed {{{update_ref_to_start}}} to {{{remove_lead_entries}}} 
      since it now updates other state in the window besides
      {{{ref_to_window_start}}} (as it should).
    
      5. Fixed various bugs in {{{remove_lead_entries}}}.  (That's why I
      put in {{{CHECK_REP}}}.)
    
      6. Added code to maintain the {{{min}}} fields (that's what puts the
      first ``M'' in ``MMU.''  I am maintaining a {{{max}}} field as well;
      that's really just there to allow some human-driven sanity checking
      of the results.
    
    
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@5996 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- src/Rts/Sys/gc_mmu_log.c
+++ src/Rts/Sys/gc_mmu_log.c
@@ -13,13 +13,20 @@
 
 #define EXPORT
 
+#if 0
+#define CHECK_REP( log ) do { check_rep( log ); } while (0) 
+#else 
+#define CHECK_REP( log ) do {                   } while (0)
+#endif
+static void check_rep( gc_mmu_log_t *log );
+
 struct event_phase_stats {
-  unsigned current_real;
-  unsigned current_cpu;
-  unsigned min_real;
-  unsigned min_cpu;
-  unsigned max_real;
-  unsigned max_cpu;
+  int current_real;
+  int current_cpu;
+  int min_real;
+  int min_cpu;
+  int max_real;
+  int max_cpu;
 };
 
 struct ref_to_window_start {
@@ -91,14 +98,162 @@ struct gc_mmu_log {
   } windows;
 };
 
+static int check_rep_total_log_time( gc_mmu_log_t *log, bool isreal_notcpu )
+{
+  int i, count;
+  count = 0;
+  for ( i = log->buffer.first; 
+        i < log->buffer.end; 
+        i = (i+1)%log->buffer.capacity ) {
+    if (isreal_notcpu)
+      count += log->buffer.entries[i].elapsed_real;
+    else 
+      count += log->buffer.entries[i].elapsed_cpu;
+  }
+  return count;
+}
+
+static int check_rep_total_phase_time_in_window( gc_mmu_log_t *log,
+                                                 struct event_window *w,
+                                                 gc_log_phase_t phase, 
+                                                 bool isreal_notcpu )
+{
+  int i, start, end, cap, count; 
+  if (isreal_notcpu) {
+    start = w->window_start_real.buf_idx;
+  } else {
+    start = w->window_start_cpu.buf_idx;
+  }
+  if (start == -1) {
+    start = log->buffer.first;
+    count = 0;
+  } else if (log->buffer.entries[start].phase == phase) {
+    /* correct for first entry that we disregard as not
+       part of the window */
+    count = ( isreal_notcpu
+              ? - w->window_start_real.entry_offset
+              : - w->window_start_cpu.entry_offset );
+  } else {
+    count = 0;
+  }
+  end = log->buffer.end;
+  cap = log->buffer.capacity;
+
+  assert( start >= 0 );
+  assert( start < cap );
+
+  for( i = start; i != end; i = (i+1)%cap ) {
+    if (log->buffer.entries[i].phase == phase) {
+      int entry_time = ((isreal_notcpu) 
+                        ? log->buffer.entries[i].elapsed_real
+                        : log->buffer.entries[i].elapsed_cpu );
+      count += entry_time;
+    }
+  }
+  return count;
+}
+
+static void check_rep_window_start( gc_mmu_log_t *log,
+                                    struct event_window *w,
+                                    struct ref_to_window_start *s, 
+                                    bool isreal_notcpu )
+{
+  struct { 
+    int mutator;
+    int misc_memmgr;
+    int minorgc;
+    int majorgc;
+    int summarize;
+    int smircy;
+  } total_phase_time;
+
+  total_phase_time.mutator = 
+    check_rep_total_phase_time_in_window( log, w, gc_log_phase_mutator, 
+                                          isreal_notcpu );
+  total_phase_time.misc_memmgr = 
+    check_rep_total_phase_time_in_window( log, w, gc_log_phase_misc_memmgr, 
+                                          isreal_notcpu );
+  total_phase_time.minorgc = 
+    check_rep_total_phase_time_in_window( log, w, gc_log_phase_minorgc, 
+                                          isreal_notcpu );
+  total_phase_time.majorgc = 
+    check_rep_total_phase_time_in_window( log, w, gc_log_phase_majorgc, 
+                                          isreal_notcpu );
+  total_phase_time.summarize = 
+    check_rep_total_phase_time_in_window( log, w, gc_log_phase_summarize, 
+                                          isreal_notcpu );
+  total_phase_time.smircy = 
+    check_rep_total_phase_time_in_window( log, w, gc_log_phase_smircy, 
+                                          isreal_notcpu );
+
+  if (s->buf_idx == -1) {
+    int total_log_time = check_rep_total_log_time(log, isreal_notcpu);
+    /* means window is not yet filled */
+    assert( total_log_time < w->size );
+  } else {
+    assert( (total_phase_time.mutator + 
+             total_phase_time.misc_memmgr + 
+             total_phase_time.minorgc + 
+             total_phase_time.majorgc + 
+             total_phase_time.summarize + 
+             total_phase_time.smircy) == w->size );
+  }
+
+  assert( total_phase_time.mutator == 
+          ((isreal_notcpu) 
+           ? w->mutator.current_real 
+           : w->mutator.current_cpu) );
+  assert( total_phase_time.misc_memmgr == 
+          ((isreal_notcpu) 
+           ? w->misc_memmgr.current_real 
+           : w->misc_memmgr.current_cpu) );
+  assert( total_phase_time.minorgc == 
+          ((isreal_notcpu) 
+           ? w->minorgc.current_real 
+           : w->minorgc.current_cpu) );
+  assert( total_phase_time.majorgc == 
+          ((isreal_notcpu) 
+           ? w->majorgc.current_real 
+           : w->majorgc.current_cpu) );
+  assert( total_phase_time.summarize == 
+          ((isreal_notcpu) 
+           ? w->summarize.current_real 
+           : w->summarize.current_cpu) );
+  assert( total_phase_time.smircy == 
+          ((isreal_notcpu) 
+           ? w->smircy.current_real 
+           : w->smircy.current_cpu) );
+}
+
+static void check_rep_window( gc_mmu_log_t *log, struct event_window *w )
+{
+  check_rep_window_start( log, w, &w->window_start_real, TRUE );
+  check_rep_window_start( log, w, &w->window_start_cpu, FALSE );
+}
+
+static void check_rep_windows( gc_mmu_log_t *log ) 
+{
+  int i; 
+  for ( i = 0 ; i < log->windows.len; i++ ) {
+    check_rep_window( log, &log->windows.array[i] );
+  }
+}
+
+static void check_rep( gc_mmu_log_t *log ) 
+{
+  check_rep_windows( log );
+}
+
 static void clear_phase_stats( struct event_phase_stats *s )
 {
   s->current_real = 0;
   s->current_cpu  = 0;
-  s->min_real     = 0;
-  s->min_cpu      = 0;
+  s->min_real     = (int)(((unsigned)~0) >> 1);
+  s->min_cpu      = (int)(((unsigned)~0) >> 1);
   s->max_real     = 0;
   s->max_cpu      = 0;
+
+  assert( (s->min_real > 0) && (s->min_real+1 < 0) );
 }
 
 static void clear_event_window( struct event_window *e )
@@ -148,13 +303,64 @@ create_gc_mmu_log( int *window_lens, int buffer_length, gc_log_phase_t init )
     }
   }
 
+  CHECK_REP( log );
+
   return log;
+}
+
+static char*
+log_phase_name( gc_log_phase_t p ) 
+{
+  switch (p) {
+  case gc_log_phase_mutator:     return "mutator";
+  case gc_log_phase_misc_memmgr: return "misc_memmgr";
+  case gc_log_phase_minorgc:     return "minorgc";
+  case gc_log_phase_majorgc:     return "majorgc";
+  case gc_log_phase_summarize:   return "summarize";
+  case gc_log_phase_smircy:      return "smircy";
+  default: assert(0);
+  }
+}
+
+static struct event_phase_stats*
+window_phase_stats( gc_mmu_log_t *log, 
+                    struct event_window *w, gc_log_phase_t incoming )
+{
+  struct event_phase_stats *s;
+
+  switch (incoming) {
+  case gc_log_phase_mutator: 
+    s = &w->mutator; 
+    break;
+  case gc_log_phase_misc_memmgr:
+    s = &w->misc_memmgr;
+    break;
+  case gc_log_phase_minorgc:
+    s = &w->minorgc;
+    break;
+  case gc_log_phase_majorgc:
+    s = &w->majorgc;
+    break;
+  case gc_log_phase_summarize:
+    s = &w->summarize;
+    break;
+  case gc_log_phase_smircy:
+    s = &w->smircy;
+    break;
+  default: 
+    assert(0);
+  }
+
+  return s;
 }
 
 #define ENTRY_ELAPSED( isreal, entry ) \
   ((isreal)?(entry).elapsed_real:(entry).elapsed_cpu)
 
-static void update_ref_to_start( gc_mmu_log_t *log,
+/* A new event was just enqueued in w; updates window_start for w,
+ * dropping entries that now fall outside the window size, and updates
+ * the window's current times accordingly. */
+static void remove_lead_entries( gc_mmu_log_t *log,
                                  struct event_window *w, 
                                  struct ref_to_window_start *r,
                                  unsigned new_elapsed,
@@ -176,7 +382,15 @@ static void update_ref_to_start( gc_mmu_log_t *log,
   } else {
     /* (increase the window in the other direction so that it starts
      *  at the beginning of the first entry; see comment below.) */
+    struct event_phase_stats *s;
+    s = window_phase_stats( log, w, log->buffer.entries[r->buf_idx].phase );
+
     unshifted_plus_new = size + new_elapsed + r->entry_offset;
+    if (isreal_and_not_cpu) {
+      s->current_real += r->entry_offset;
+    } else {
+      s->current_cpu += r->entry_offset;
+    }
   }
 
   /* At this point, the conceptual window starts at the beginning of
@@ -189,21 +403,44 @@ static void update_ref_to_start( gc_mmu_log_t *log,
   /* Step through log until we can go no further. */
   { 
     int i, shifted_plus_new, elapsed;
-    i = log->buffer.first;
+    gc_log_phase_t phase;
+    struct event_phase_stats *s;
+    i = r->buf_idx;
+    if (i == -1)
+      i = log->buffer.first;
     shifted_plus_new = unshifted_plus_new;
     elapsed = ENTRY_ELAPSED( isreal_and_not_cpu, log->buffer.entries[i] );
-    while ((shifted_plus_new - elapsed) > size) {
+    phase = log->buffer.entries[i].phase;
+    while ((shifted_plus_new - elapsed) >= size) {
       assert( i != log->buffer.end );
       shifted_plus_new -= elapsed;
+      s = window_phase_stats( log, w, phase );
+
+      if (isreal_and_not_cpu) {
+        s->current_real -= elapsed;
+      } else {
+        s->current_cpu  -= elapsed;
+      }
       i = ((i+1) % log->buffer.capacity);
       elapsed = ENTRY_ELAPSED(isreal_and_not_cpu, log->buffer.entries[i]);
+      phase = log->buffer.entries[i].phase;
     }
     assert( elapsed == ENTRY_ELAPSED( isreal_and_not_cpu, log->buffer.entries[i] ));
-    assert( (shifted_plus_new - elapsed) <= size );
+    assert( (shifted_plus_new - elapsed) < size );
     assert( shifted_plus_new >= size );
-
-    r->buf_idx = i;
-    r->entry_offset = size - (shifted_plus_new - elapsed);
+    { 
+      struct event_phase_stats *s;
+      int partial_elapsed;
+      s = window_phase_stats( log, w, phase );
+      partial_elapsed = shifted_plus_new - size;
+      if (isreal_and_not_cpu) {
+        s->current_real -= partial_elapsed;
+      } else {
+        s->current_cpu  -= partial_elapsed;
+      }
+      r->buf_idx = i;
+      r->entry_offset = partial_elapsed;
+    }
   }
 }
 static int buf_min( gc_mmu_log_t *log, int idx_1, int idx_2 ) 
@@ -236,8 +473,8 @@ static int update_window_drop_outdated( gc_mmu_log_t *log,
 {
   int new_buf_idx_real, new_buf_idx_cpu;
 
-  update_ref_to_start( log, w, &w->window_start_real, elapsed_real, TRUE );
-  update_ref_to_start( log, w, &w->window_start_cpu,  elapsed_cpu, FALSE );
+  remove_lead_entries( log, w, &w->window_start_real, elapsed_real, TRUE );
+  remove_lead_entries( log, w, &w->window_start_cpu,  elapsed_cpu, FALSE );
 
   new_buf_idx_real = w->window_start_real.buf_idx;
   new_buf_idx_cpu  = w->window_start_cpu.buf_idx;
@@ -275,6 +512,80 @@ static void update_windows_drop_outdated( gc_mmu_log_t *log,
   log->buffer.first = choose_new_first;
 }
 
+static void update_phase_stats_add( gc_mmu_log_t *log,
+                                    struct event_window *w,
+                                    struct event_phase_stats *s,
+                                    unsigned elapsed_real, 
+                                    unsigned elapsed_cpu ) 
+{
+  assert2( s->current_real >= 0 );
+  assert2( s->current_cpu  >= 0 );
+
+  s->current_real += elapsed_real;
+  s->current_cpu  += elapsed_cpu;
+
+  assert2( s->current_real >= 0 );
+  assert2( s->current_cpu  >= 0 );
+}
+
+static void update_window_incoming_event( gc_mmu_log_t *log,
+                                          struct event_window *w, 
+                                          gc_log_phase_t incoming,
+                                          unsigned elapsed_real,
+                                          unsigned elapsed_cpu )
+{
+  struct event_phase_stats *s;
+  s = window_phase_stats( log, w, incoming );
+  update_phase_stats_add( log, w, s, elapsed_real, elapsed_cpu );
+}
+
+static void update_windows_incoming_event( gc_mmu_log_t *log,
+                                           gc_log_phase_t incoming,
+                                           unsigned elapsed_real,
+                                           unsigned elapsed_cpu )
+{
+  struct event_window *w;
+  int i;
+  for ( i = 0; i < log->windows.len; i++ ) {
+    w = &log->windows.array[i];
+    update_window_incoming_event( log, w, incoming, elapsed_real, elapsed_cpu );
+  }
+}
+
+static void phase_stats_recalc_minmax( gc_mmu_log_t *log,
+                                       struct event_window *w, 
+                                       struct event_phase_stats *s )
+{
+  s->min_real = min( s->current_real, s->min_real );
+  s->min_cpu  = min( s->current_cpu,  s->min_cpu );
+  s->max_real = max( s->current_real, s->max_real );
+  s->max_cpu  = max( s->current_cpu,  s->max_cpu );
+}
+
+static void update_window_recalc_minmax( gc_mmu_log_t *log, 
+                                         struct event_window *w) 
+{
+  phase_stats_recalc_minmax( log, w, &w->mutator );
+  phase_stats_recalc_minmax( log, w, &w->misc_memmgr );
+  phase_stats_recalc_minmax( log, w, &w->minorgc );
+  phase_stats_recalc_minmax( log, w, &w->majorgc );
+  phase_stats_recalc_minmax( log, w, &w->summarize );
+  phase_stats_recalc_minmax( log, w, &w->smircy );
+}
+
+static void update_windows_recalc_minmax( gc_mmu_log_t *log ) 
+{
+  struct event_window *w;
+  int i;
+  for ( i = 0; i < log->windows.len; i++ ) {
+    w = &log->windows.array[i];
+    if (w->window_start_real.buf_idx != -1 
+        && w->window_start_cpu.buf_idx != -1) {
+      update_window_recalc_minmax( log, w );
+    }
+  }
+}
+
 static void enqueue( gc_mmu_log_t *log,
                      gc_log_phase_t incoming,
                      unsigned elapsed_real,
@@ -286,6 +597,8 @@ static void enqueue( gc_mmu_log_t *log,
   log->buffer.entries[enq].elapsed_real = elapsed_real;
   log->buffer.entries[enq].elapsed_cpu  = elapsed_cpu;
 
+  update_windows_incoming_event( log, incoming, elapsed_real, elapsed_cpu );
+
   enq = (enq+1) % log->buffer.capacity;
   if ( enq == log->buffer.first ) {
     panic_exit( "Cannot enqueue event in MMU log of capacity %d entries "
@@ -295,6 +608,7 @@ static void enqueue( gc_mmu_log_t *log,
   log->buffer.end = enq;
 
   update_windows_drop_outdated( log, incoming, elapsed_real, elapsed_cpu );
+  update_windows_recalc_minmax( log );
 }
 
 EXPORT
@@ -305,6 +619,8 @@ void gc_mmu_log_phase_shift( gc_mmu_log_t *log,
   unsigned finis_cpu  = osdep_cpuclock();
   unsigned start_real, start_cpu;
   unsigned elapsed_real, elapsed_cpu;
+
+  CHECK_REP( log );
 
   assert( log->in_progress.phase == prev );
   assert( prev != next );
@@ -323,6 +639,8 @@ void gc_mmu_log_phase_shift( gc_mmu_log_t *log,
   log->in_progress.phase = next;
   log->in_progress.start_real = start_real;
   log->in_progress.start_cpu  = start_cpu;
+
+  CHECK_REP( log );
 }
 
 static void print_event_stats( gc_mmu_log_t *log, struct event_window *w, 
