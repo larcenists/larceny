72f528d5adc1b75de4069ae67320d07ddec964c8
pnkfelix <pnkfelix@c9681fa6-b8ae-40c0-a6da-707a35ad5973>
2010-12-12 23:19:56 +0000

    Added new basic operation to remove individual entries without performing full enumeration.
    git-svn-id: https://trac.ccs.neu.edu/svn/larceny/trunk/larceny_src@6479 c9681fa6-b8ae-40c0-a6da-707a35ad5973

--- src/Rts/Sys/remset.c
+++ src/Rts/Sys/remset.c
@@ -299,6 +299,28 @@ static void handle_overflow( remset_t *rs, unsigned recorded, word *pooltop )
   assert( DATA(rs)->curr_pool != 0 );
 }
 
+void rs_del_elem( remset_t *rs, word w ) 
+{
+  word mask, *tbl, *b, *pooltop, *poollim, tblsize, h;
+  bool overflowed = FALSE;
+  remset_data_t *data = DATA(rs);
+  pooltop = data->curr_pool->top;
+  poollim = data->curr_pool->lim;
+  tbl = data->tbl_bot;
+  tblsize = data->tbl_lim - tbl;
+  mask = tblsize-1;
+
+  h = hash_object( w, mask );
+  b = (word*)tbl[ h ];
+  while (b != 0 && *b != w) {
+    b = (word*)*(b+1);
+  }
+  if (b != 0) {
+    *b = (word)(word*)0;
+    rs->live -= 1;
+  }
+}
+
 bool rs_add_elem_new( remset_t *rs, word w ) 
 {
   word mask, *tbl, *b, *pooltop, *poollim, tblsize, h;
