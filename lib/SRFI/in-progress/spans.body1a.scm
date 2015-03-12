58c46e8affee2430be6e643c4e0f9961ea957ec7
WillClinger <will@ccs.neu.edu>
2015-03-12 13:37:45 -0400

    Added first prototype of implementation for character spans.

---
+++ lib/SRFI/in-progress/spans.body1a.scm
@@ -0,0 +1,263 @@
+;;; The representation-dependent part of an implementation of
+;;; character spans that represents character spans by strings
+;;; and cursors by exact integers.
+;;;
+;;; This implementation might be suitable for systems that
+;;; don't allow non-Ascii characters in strings, don't support
+;;; bytevectors, and don't much care about performance.
+
+;;; FIXME:  So far as I can see, nothing in the specification
+;;; of character spans precludes their representation as strings.
+;;; To provide a small degree of sanity checking, however, this
+;;; implementation uses an uncommon Ascii character to tag the
+;;; strings that represent character spans.
+
+(define *span-tag*
+  (integer->char 23))   ; ^W, End of Transmission Block
+
+;;; Nothing in the specification of span cursors precludes their
+;;; representation as exact integers.  To provide a small degree
+;;; of sanity checking, this implementation adds a fixed offset
+;;; so span cursors will be less likely to be valid indexes into
+;;; a related string.
+
+(define *cursor-offset* -1000)
+
+;;; Procedures that aren't exported but may be called by the
+;;; representation-independent part of the implementation.
+
+(define (%span? x)
+  (and (string? x)
+       (< 0 (string-length x))
+       (char=? *span-tag* (string-ref x 0))))
+
+(define (%string->span str)
+  (list->string (cons *span-tag* (string->list str))))
+
+(define (%span->string sp)
+  (%check-span sp '%span->string)
+  (substring sp 1 (string-length sp)))
+
+(define (%check-span sp name)
+  (if (not (%span? sp))
+      (error (string-append (cond ((symbol? name)
+                                   (string-append (symbol->string name) ": "))
+                                  ((string? name)
+                                   (string-append name ": "))
+                                  (else ""))
+                            "not a span")
+             sp)))
+
+;;; Exported procedures.
+
+;;; String cursors.
+
+(define (string-cursor-start str)
+  (+ 0 *cursor-offset*))
+
+(define (span-cursor-start sp)
+  (+ 0 *cursor-offset*))
+
+(define (string-cursor-end str)
+  (+ (string-length str) *cursor-offset*))
+
+(define (span-cursor-end sp)
+  (+ (string-length (%span->string sp)) *cursor-offset*))
+
+(define (string-cursor-ref str curs)
+  (string-ref str (string-cursor->index str curs)))
+
+(define (span-cursor-ref sp curs)
+  (string-ref (%span->string sp) (span-cursor->index sp curs)))
+
+(define (string-cursor-next str curs)
+  (min (string-cursor-end str) (+ curs 1)))
+
+(define (span-cursor-next sp curs)
+  (min (span-cursor-end sp) (+ curs 1)))
+
+(define (string-cursor-prev str curs)
+  (max (+ -1 *cursor-offset*) (- curs 1)))
+
+(define (span-cursor-prev sp curs)
+  (max (+ -1 *cursor-offset*) (- curs 1)))
+
+(define (string-cursor-forward str curs n)
+  (min (string-cursor-end str) (+ curs n)))
+
+(define (span-cursor-forward sp curs n)
+  (min (span-cursor-end sp) (+ curs n)))
+
+(define (string-cursor-backward str curs n)
+  (max (+ -1 *cursor-offset*) (- curs n)))
+
+(define (span-cursor-backward sp curs n)
+  (max (+ -1 *cursor-offset*) (- curs n)))
+
+(define (string-cursor-forward-until str curs pred)
+  (let ((n (string-length str)))
+    (let loop ((i (string-cursor->index str curs)))
+      (cond ((>= i n)
+             (string-cursor-end str))
+            ((and (<= 0 i)
+                  (pred (string-ref str i)))
+             (string-index->cursor str i))
+            (else
+             (loop (+ i 1)))))))
+
+(define (span-cursor-forward-until sp curs pred)
+  (let* ((str (%span->string sp))
+         (n (string-length str)))
+    (let loop ((i (span-cursor->index sp curs)))
+      (cond ((>= i n)
+             (span-cursor-end sp))
+            ((and (<= 0 i)
+                  (pred (string-ref str i)))
+             (span-index->cursor sp i))
+            (else
+             (loop (+ i 1)))))))
+
+(define (string-cursor-backward-until str curs pred)
+  (let ((n (string-length str)))
+    (let loop ((i (string-cursor->index str curs)))
+      (cond ((< i 0)
+             (string-cursor-prev (string-cursor-start str)))
+            ((and (< i n)
+                  (pred (string-ref str i)))
+             (string-index->cursor str i))
+            (else
+             (loop (- i 1)))))))
+
+(define (span-cursor-backward-until sp curs pred)
+  (let* ((str (%span->string sp))
+         (n (string-length str)))
+    (let loop ((i (span-cursor->index str curs)))
+      (cond ((< i 0)
+             (span-cursor-prev (span-cursor-start sp)))
+            ((and (< i n)
+                  (pred (string-ref str i)))
+             (span-index->cursor sp i))
+            (else
+             (loop (- i 1)))))))
+
+(define (string-cursor=? str curs1 curs2)
+  (= curs1 curs2))
+
+(define (span-cursor=? sp curs1 curs2)
+  (= curs1 curs2))
+
+(define (string-cursor<? str curs1 curs2)
+  (< curs1 curs2))
+
+(define (span-cursor<? sp curs1 curs2)
+  (< curs1 curs2))
+
+(define (string-cursor>? str curs1 curs2)
+  (> curs1 curs2))
+
+(define (span-cursor>? sp curs1 curs2)
+  (> curs1 curs2))
+
+(define (string-cursor<=? str curs1 curs2)
+  (<= curs1 curs2))
+
+(define (span-cursor<=? sp curs1 curs2)
+  (<= curs1 curs2))
+
+(define (string-cursor>=? str curs1 curs2)
+  (>= curs1 curs2))
+
+(define (span-cursor>=? sp curs1 curs2)
+  (>= curs1 curs2))
+
+(define (string-cursor->index str curs)
+  (- curs *cursor-offset*))
+
+(define (span-cursor->index sp curs)
+  (- curs *cursor-offset*))
+
+(define (string-index->cursor str idx)
+  (+ idx *cursor-offset*))
+
+(define (span-index->cursor sp idx)
+  (+ idx *cursor-offset*))
+
+(define (string-cursor-difference str curs1 curs2)
+  (- curs1 curs2))
+
+(define (span-cursor-difference sp curs1 curs2)
+  (- curs1 curs2))
+
+;;; Span constructors.
+
+(define (make-whole-span str)
+  (%string->span str))
+
+(define (make-span str start end)
+  (%string->span (substring str start end)))
+
+(define (span . chars)
+  (%string->span (list->string chars)))
+
+;;; Predicates.
+
+(define (span? x)
+  (%span? x))
+
+;;; Selection.
+
+(define (span-ref sp k)
+  (string-ref (%span->string sp) k))
+
+(define (subspan sp start end)
+  (%string->span (substring (%span->string sp) start end)))
+
+(define (subspan/cursors sp start end)
+  (subspan sp
+           (span-cursor->index sp start)
+           (span-cursor->index sp end)))
+
+;;; Padding, trimming, and compressing.
+
+(define (%space? x)
+  (eqv? x #\space))
+
+(define (%span-trim-predicate rest)
+  (if (null? rest)
+      %space?
+      (car rest)))
+
+(define (span-trim sp . rest)
+  (let ((pred (%span-trim-predicate rest)))
+    (let loop ((chars (string->list (%span->string sp))))
+      (cond ((null? chars)
+             (%string->span ""))
+            ((pred (car chars))
+             (loop (cdr chars)))
+            (else
+             (list->span chars))))))
+
+(define (span-trim-right sp . rest)
+  (let ((pred (%span-trim-predicate rest)))
+    (let loop ((chars (reverse (string->list (%span->string sp)))))
+      (cond ((null? chars)
+             (%string->span ""))
+            ((pred (car chars))
+             (loop (cdr chars)))
+            (else
+             (list->span (reverse chars)))))))
+
+;;; The whole character span or string.
+
+(define (span-length sp)
+  (string-length (%span->string sp)))
+
+;;; Conversion.
+
+(define (string->span str)
+  (%string->span str))
+
+(define (span->string sp)
+  (%span->string sp))
+
+; eof
---
+++ lib/SRFI/in-progress/spans.sld
@@ -0,0 +1,206 @@
+;;; Prototype implementation of a character span library as specified at
+;;; http://trac.sacrideo.us/wg/wiki/CharacterSpansCowan
+;;;
+;;; That document says
+;;;
+;;;     The sample implementation (which is not yet written) represents
+;;;     spans as records containing a string and two string cursors, and
+;;;     provides two implementations of string cursors, one using string
+;;;     indexes directly and one that layers UTF-8 character spans on top
+;;;     of single-byte native strings.
+;;;
+;;; This implementation does not fulfill that promise.  Instead, it has
+;;; two implementations that represent spans and cursors as follows:
+;;;
+;;;     as strings and small exact integers (-1 and the valid indexes)
+;;;     as bytevectors and small exact integers (-1 and valid indexes)
+;;;
+;;; In both implementations, the string or bytevector is hidden inside
+;;; a record.
+;;;
+;;; Rationale:  There is no guarantee that an implementation's single-byte
+;;; native strings are capable of representing every bit pattern needed by
+;;; UTF-8, so the second of the promised sample implementations involves a
+;;; reinvention of something like UTF-8 with one less bit.  Even that would
+;;; assume the single-byte native strings can represent full Ascii, but
+;;; some systems discriminate against the nul character.  Implementations
+;;; that don't support bytevectors can use vectors instead and pay the
+;;; price in space efficiency.
+
+(define-library (in-progress spans)
+
+  (export
+
+   string-cursor-start
+   span-cursor-start
+   string-cursor-end
+   span-cursor-end
+   string-cursor-ref
+   span-cursor-ref
+   string-cursor-next
+   span-cursor-next
+   string-cursor-prev
+   span-cursor-prev
+   string-cursor-forward
+   span-cursor-forward
+   string-cursor-backward
+   span-cursor-backward
+   string-cursor-forward-until
+   span-cursor-forward-until
+   string-cursor-backward-until
+   span-cursor-backward-until
+
+   string-cursor=?
+   span-cursor=?
+   string-cursor<?
+   span-cursor<?
+   string-cursor>?
+   span-cursor>?
+   string-cursor<=?
+   span-cursor<=?
+   string-cursor>=?
+   span-cursor>=?
+
+   string-cursor->index
+   span-cursor->index
+   string-index->cursor
+   span-index->cursor
+   string-cursor-difference
+   span-cursor-difference
+
+   make-whole-span
+   make-span
+   span
+   span-transform
+   span-unfold
+   span-unfold-right
+   span-tabulate                ; deprecated because of argument order
+
+   span?
+   span-null?
+   span-every
+   span-any
+
+   span-ref
+   span-take
+   span-take-right
+   span-drop
+   span-drop-right
+   span-split-at
+   span-replicate
+   subspan
+   subspan/cursors
+
+   span-pad
+   span-pad-right
+   span-trim
+   span-trim-right
+   span-trim-both
+   span-compress
+
+   span-prefix
+   span-suffix
+   span-prefix-length
+   span-suffix-length
+   span-mismatch
+   span-mismatch-right
+   span-prefix?
+   span-suffix?
+
+   span-count
+   span-find
+   span-find-right
+   span-skip
+   span-skip-right
+   span-take-while
+   span-drop-while
+   span-span
+   span-break
+   span-contains
+
+   span-length
+   span-reverse
+   span-append
+   span-concatenate
+   span-concatenate-reverse
+
+   span-map
+   span-for-each
+   span-fold
+   span-fold-right
+
+   span-split
+   span-join
+
+   span-filter
+   span-remove
+   span-partition
+
+   string->span
+   span->string
+   span->list
+   span->vector
+   list->span
+   vector->span
+   reverse-list->span
+
+   span-upcase
+   span-downcase
+   span-foldcase
+
+   span=?
+   span<?
+   span>?
+   span<=?
+   span>=?
+   span-ci=?
+   span-ci<?
+   span-ci>?
+   span-ci<=?
+   span-ci>=?
+
+   span-comparator
+   )
+
+  (import (scheme base)
+          (scheme char)
+          (srfi 114))
+
+  ;; A sane mod procedure is needed by span-replicate.
+
+  (cond-expand
+   ((library (rnrs base))
+    (import (only (rnrs base) mod)))
+   (else
+    (begin
+     (define (mod x y)
+       (cond ((and (exact-integer? x)
+                   (exact-integer? y)
+                   (>= x 0))
+              (remainder x y))
+             ((< y 0)
+              ; x < 0, y < 0
+              (let* ((q (quotient x y))
+                     (r (- x (* q y))))
+                (if (= r 0)
+                    0
+                    (- r y))))
+             (else
+              ; x < 0, y > 0
+              (let* ((q (quotient x y))
+                     (r (- x (* q y))))
+                (if (= r 0)
+                    0
+                    (+ r y)))))))))
+
+  ;; The representation-dependent part of the implementation.
+
+  (include "spans.body1.scm")
+
+  ;; The representation-independent part of the implementation.
+
+  (include "spans.body2.scm")
+
+  )
+
+; eof
