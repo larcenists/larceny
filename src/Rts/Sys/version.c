6a47a7189d0620e4ab9f372fba9fb4f78ba5970c
WillClinger <will@ccs.neu.edu>
2016-05-08 20:27:00 -0400

    Bumped version to 0.99a1.

--- src/Rts/Sys/version.c
+++ src/Rts/Sys/version.c
@@ -69,9 +69,9 @@
 #include "config.h"
 
 int  larceny_major_version = 0;
-int  larceny_minor_version = 98;
+int  larceny_minor_version = 99;
 /* char *larceny_version_qualifier = " \"General Ripper\""; */
-char *larceny_version_qualifier = "+ \"General Ripper\"";
+char *larceny_version_qualifier = "a1 (alpha test)";
 
 char *date = DATE " " TIME;
 char *user = USER;
