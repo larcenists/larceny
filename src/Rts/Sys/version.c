23b93350dd5be805345759058160bdba9e08baac
WillClinger <will@ccs.neu.edu>
2017-07-25 22:41:38 -0400

    Bumped version to 1.3b6

--- src/Rts/Sys/version.c
+++ src/Rts/Sys/version.c
@@ -72,7 +72,7 @@
 int  larceny_major_version = 1;
 int  larceny_minor_version = 3;
 /* char *larceny_version_qualifier = " \"Grain Alcohol and Rainwater\""; */
-char *larceny_version_qualifier = "a5";
+char *larceny_version_qualifier = "b6";
 
 char *date = DATE " " TIME;
 char *user = USER;
